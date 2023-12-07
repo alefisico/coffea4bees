246cd71084b2bf228f872c4d6b7b83cf8bfe3b6a
Alejandro <gomez@rutgers.physics.edu>
2023-12-07 16:12:02 -0600

    adding reproducible metadata

--- python/analysis/runTestJob.sh
+++ python/analysis/runTestJob.sh
@@ -1,1 +1,1 @@
-python runner.py -t -o test.coffea -d HH4b mixeddata -op analysis/hists/ -p analysis/processors/processor_HH4b.py
+python runner.py -t -o test.coffea -d HH4b data -op analysis/hists/ -p analysis/processors/processor_HH4b.py
