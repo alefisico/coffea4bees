e2841a9a65908d47dfd3987f3e0f499b2a12b178
John Alison <johnalison@cmu.edu>
2023-12-09 22:25:43 -0600

    Update diff and hash

--- python/base_class/addhash.py
+++ python/base_class/addhash.py
@@ -3,6 +3,12 @@ import subprocess
 def get_git_revision_short_hash() -> str:
     return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
 
+def get_git_revision_hash() -> str:
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
+
+
 def get_git_diff_master() -> str:
-    ps = subprocess.Popen(['git', 'diff', 'origin/master', 'HEAD'], stdout=subprocess.PIPE)
-    return subprocess.check_output(('base64', '-w', '0'), stdin=ps.stdout).decode("ascii")
+    return subprocess.check_output(['git', 'diff', 'origin/master', 'HEAD']).decode('ascii')
+
+def get_git_diff() -> str:
+    return subprocess.check_output(['git', 'diff']).decode('ascii')
