7f3fdcde2e835b212e2f3ca3be79d556a37e09b9
John Alison <johnda102@gmail.com>
2023-12-09 23:04:25 -0500

    Add all data cutflows

--- python/analysis/tests/cutflow_test.py
+++ python/analysis/tests/cutflow_test.py
@@ -16,11 +16,19 @@ class CutFlowTestCase(unittest.TestCase):
         self.cf3_unit = hists["cutFlowThreeTagUnitWeight"]
 
         self.counts4 = {}
+
+        self.counts4['data_UL16_preVFPB'] = {'passJetMult' : 598690, 'passPreSel' : 17900.0, 'passDiJetMass' : 9211.0, 'SR' : 3424.0, 'SB' : 5787.0, 'passSvB' : 15.0, 'failSvB' : 10432.0, }
+        self.counts4['data_UL16_preVFPC'] = {'passJetMult' : 256268, 'passPreSel' : 7263.0, 'passDiJetMass' : 3721.0, 'SR' : 1344.0, 'SB' : 2377.0, 'passSvB' : 4.0, 'failSvB' : 4229.0, }
+        self.counts4['data_UL16_preVFPD'] = {'passJetMult' : 421098, 'passPreSel' : 11270.0, 'passDiJetMass' : 5909.0, 'SR' : 2115.0, 'SB' : 3794.0, 'passSvB' : 9.0, 'failSvB' : 6595.0, }
+        self.counts4['data_UL16_preVFPE'] = {'passJetMult' : 395976, 'passPreSel' : 9779.0, 'passDiJetMass' : 4964.0, 'SR' : 1810.0, 'SB' : 3154.0, 'passSvB' : 2.0, 'failSvB' : 5762.0, }
+        self.counts4['data_UL16_postVFPF'] = {'passJetMult' : 311677, 'passPreSel' : 7731.0, 'passDiJetMass' : 3964.0, 'SR' : 1449.0, 'SB' : 2515.0, 'passSvB' : 6.0, 'failSvB' : 4549.0, }
+        self.counts4['data_UL16_postVFPG'] = {'passJetMult' : 877903, 'passPreSel' : 22800.0, 'passDiJetMass' : 12090.0, 'SR' : 4439.0, 'SB' : 7651.0, 'passSvB' : 11.0, 'failSvB' : 13699.0, }
+        self.counts4['data_UL16_postVFPH'] = {'passJetMult' : 903644, 'passPreSel' : 24327.0, 'passDiJetMass' : 12698.0, 'SR' : 4676.0, 'SB' : 8022.0, 'passSvB' : 16.0, 'failSvB' : 14548.0, }
+
         self.counts4["data_UL17C"] = {'passJetMult': 498557, 'passPreSel': 26897,   'passDiJetMass': 12237,   'SR': 4424,   'SB': 7813,   'passSvB': 12,   'failSvB': 15156}
         self.counts4['data_UL17D'] = {'passJetMult': 234161, 'passPreSel': 12959.0, 'passDiJetMass': 5904.0,  'SR': 2065.0, 'SB': 3839.0, 'passSvB': 7.0,  'failSvB': 7372.0}
         self.counts4['data_UL17E'] = {'passJetMult': 549026, 'passPreSel': 26873.0, 'passDiJetMass': 12165.0, 'SR': 4308.0, 'SB': 7857.0, 'passSvB': 14.0, 'failSvB': 15622.0}
         self.counts4['data_UL17F'] = {'passJetMult': 710245, 'passPreSel': 27712.0, 'passDiJetMass': 12225.0, 'SR': 4478.0, 'SB': 7747.0, 'passSvB': 14.0, 'failSvB': 15810.0}
-
 
         self.counts4['data_UL18A'] = {'passJetMult': 902005,  'passPreSel': 34344.0, 'passDiJetMass': 14465.0, 'SR': 5486.0,  'SB': 8979.0,  'passSvB': 27.0, 'failSvB': 20510.0}
         self.counts4['data_UL18B'] = {'passJetMult': 448667,  'passPreSel': 16933.0, 'passDiJetMass': 7028.0,  'SR': 2648.0,  'SB': 4380.0,  'passSvB': 8.0,  'failSvB': 10048.0}
@@ -29,6 +37,15 @@ class CutFlowTestCase(unittest.TestCase):
         
         
         self.counts3 = {}
+
+        self.counts3['data_UL16_preVFPB'] = {'passJetMult' : 598690, 'passPreSel' : 15419.8, 'passDiJetMass' : 8212.1, 'SR' : 2951.2, 'SB' : 5260.9, 'passSvB' : 12.6, 'failSvB' : 9018.7, }
+        self.counts3['data_UL16_preVFPC'] = {'passJetMult' : 256268, 'passPreSel' : 6541.5, 'passDiJetMass' : 3501.8, 'SR' : 1270.1, 'SB' : 2231.7, 'passSvB' : 4.0, 'failSvB' : 3854.6, }
+        self.counts3['data_UL16_preVFPD'] = {'passJetMult' : 421098, 'passPreSel' : 10871.5, 'passDiJetMass' : 5832.4, 'SR' : 2110.2, 'SB' : 3722.2, 'passSvB' : 8.0, 'failSvB' : 6405.0, }
+        self.counts3['data_UL16_preVFPE'] = {'passJetMult' : 395976, 'passPreSel' : 9943.5, 'passDiJetMass' : 5239.4, 'SR' : 1875.2, 'SB' : 3364.2, 'passSvB' : 6.0, 'failSvB' : 5877.9, }
+        self.counts3['data_UL16_postVFPF'] = {'passJetMult' : 311677, 'passPreSel' : 7745.4, 'passDiJetMass' : 4123.3, 'SR' : 1497.4, 'SB' : 2625.8, 'passSvB' : 4.6, 'failSvB' : 4600.4, }
+        self.counts3['data_UL16_postVFPG'] = {'passJetMult' : 877903, 'passPreSel' : 21786.4, 'passDiJetMass' : 11875.5, 'SR' : 4318.6, 'SB' : 7556.8, 'passSvB' : 11.8, 'failSvB' : 13001.2, }
+        self.counts3['data_UL16_postVFPH'] = {'passJetMult' : 903644, 'passPreSel' : 22182.7, 'passDiJetMass' : 12038.8, 'SR' : 4399.5, 'SB' : 7639.3, 'passSvB' : 11.6, 'failSvB' : 13194.5, }
+
         self.counts3['data_UL17C'] = {'passJetMult': 498557, 'passPreSel': 21919.6, 'passDiJetMass': 10567.7, 'SR': 3784.1, 'SB': 6783.6, 'passSvB': 15.7, 'failSvB': 12086.0}
         self.counts3['data_UL17D'] = {'passJetMult': 234161, 'passPreSel': 10307.4, 'passDiJetMass': 4987.6,  'SR': 1769.5, 'SB': 3218.0, 'passSvB': 5.1,  'failSvB': 5698.5}
         self.counts3['data_UL17E'] = {'passJetMult': 549026, 'passPreSel': 22916.2, 'passDiJetMass': 11071.6, 'SR': 4000.6, 'SB': 7071.0, 'passSvB': 12.2, 'failSvB': 12947.6}
@@ -38,10 +55,18 @@ class CutFlowTestCase(unittest.TestCase):
         self.counts3['data_UL18B'] = {'passJetMult': 448667, 'passPreSel': 15304.0, 'passDiJetMass': 6769.8,  'SR': 2559.6,  'SB': 4210.2,  'passSvB': 12.3, 'failSvB': 8846.6}
         self.counts3['data_UL18C'] = {'passJetMult': 418436, 'passPreSel': 14210.1, 'passDiJetMass': 6275.6,  'SR': 2365.5,  'SB': 3910.2,  'passSvB': 11.7, 'failSvB': 8213.2}
         self.counts3['data_UL18D'] = {'passJetMult': 1874829,'passPreSel': 64693.0, 'passDiJetMass': 28721.1, 'SR': 10774.5, 'SB': 17946.7, 'passSvB': 50.7, 'failSvB': 37086.3}
-
         
 
         self.counts3_unit = {}
+
+        self.counts3_unit['data_UL16_preVFPB'] = {'passJetMult' : 598690, 'passPreSel' : 580790, 'passDiJetMass' : 241400, 'SR' : 91410, 'SB' : 149990, 'passSvB' : 256, 'failSvB' : 369711, }
+        self.counts3_unit['data_UL16_preVFPC'] = {'passJetMult' : 256268, 'passPreSel' : 249005, 'passDiJetMass' : 104103, 'SR' : 39637, 'SB' : 64466, 'passSvB' : 86, 'failSvB' : 159763, }
+        self.counts3_unit['data_UL16_preVFPD'] = {'passJetMult' : 421098, 'passPreSel' : 409828, 'passDiJetMass' : 172162, 'SR' : 65369, 'SB' : 106793, 'passSvB' : 174, 'failSvB' : 262289, }
+        self.counts3_unit['data_UL16_preVFPE'] = {'passJetMult' : 395976, 'passPreSel' : 386197, 'passDiJetMass' : 158243, 'SR' : 59753, 'SB' : 98490, 'passSvB' : 130, 'failSvB' : 248662, }
+        self.counts3_unit['data_UL16_postVFPF'] = {'passJetMult' : 311677, 'passPreSel' : 303946, 'passDiJetMass' : 126079, 'SR' : 47844, 'SB' : 78235, 'passSvB' : 114, 'failSvB' : 196811, }
+        self.counts3_unit['data_UL16_postVFPG'] = {'passJetMult' : 877903, 'passPreSel' : 855103, 'passDiJetMass' : 368335, 'SR' : 139637, 'SB' : 228698, 'passSvB' : 244, 'failSvB' : 554649, }
+        self.counts3_unit['data_UL16_postVFPH'] = {'passJetMult' : 903644, 'passPreSel' : 879317, 'passDiJetMass' : 375509, 'SR' : 142948, 'SB' : 232561, 'passSvB' : 261, 'failSvB' : 569771, }
+        
         self.counts3_unit['data_UL17C'] = {'passJetMult': 498557, 'passPreSel': 471660, 'passDiJetMass': 160816, 'SR': 60898, 'SB': 99918,  'passSvB': 181, 'failSvB': 288362}
         self.counts3_unit['data_UL17D'] = {'passJetMult': 234161, 'passPreSel': 221202, 'passDiJetMass': 76166,  'SR': 28620, 'SB': 47546,  'passSvB': 65,  'failSvB': 135202}
         self.counts3_unit['data_UL17E'] = {'passJetMult': 549026, 'passPreSel': 522153, 'passDiJetMass': 179149, 'SR': 67871, 'SB': 111278, 'passSvB': 168, 'failSvB': 327319}
@@ -51,14 +76,6 @@ class CutFlowTestCase(unittest.TestCase):
         self.counts3_unit['data_UL18B'] = {'passJetMult': 448667,  'passPreSel': 431734,  'passDiJetMass': 131403, 'SR': 52148,  'SB': 79255,  'passSvB': 209, 'failSvB': 277130}
         self.counts3_unit['data_UL18C'] = {'passJetMult': 418436,  'passPreSel': 402678,  'passDiJetMass': 121773, 'SR': 47810,  'SB': 73963,  'passSvB': 167, 'failSvB': 259193}
         self.counts3_unit['data_UL18D'] = {'passJetMult': 1874829, 'passPreSel': 1804145, 'passDiJetMass': 555111, 'SR': 218599, 'SB': 336512, 'passSvB': 802, 'failSvB': 1149231}        
-
-        self.counts3_unit['data_UL16_preVFPB'] = {'passJetMult' : 598690, 'passPreSel' : 580790, 'passDiJetMass' : 241400, 'SR' : 91410, 'SB' : 149990, 'passSvB' : 256, 'failSvB' : 369711, }
-        self.counts3_unit['data_UL16_preVFPC'] = {'passJetMult' : 256268, 'passPreSel' : 249005, 'passDiJetMass' : 104103, 'SR' : 39637, 'SB' : 64466, 'passSvB' : 86, 'failSvB' : 159763, }
-        self.counts3_unit['data_UL16_preVFPD'] = {'passJetMult' : 421098, 'passPreSel' : 409828, 'passDiJetMass' : 172162, 'SR' : 65369, 'SB' : 106793, 'passSvB' : 174, 'failSvB' : 262289, }
-        self.counts3_unit['data_UL16_preVFPE'] = {'passJetMult' : 395976, 'passPreSel' : 386197, 'passDiJetMass' : 158243, 'SR' : 59753, 'SB' : 98490, 'passSvB' : 130, 'failSvB' : 248662, }
-        self.counts3_unit['data_UL16_postVFPF'] = {'passJetMult' : 311677, 'passPreSel' : 303946, 'passDiJetMass' : 126079, 'SR' : 47844, 'SB' : 78235, 'passSvB' : 114, 'failSvB' : 196811, }
-        self.counts3_unit['data_UL16_postVFPG'] = {'passJetMult' : 877903, 'passPreSel' : 855103, 'passDiJetMass' : 368335, 'SR' : 139637, 'SB' : 228698, 'passSvB' : 244, 'failSvB' : 554649, }
-        self.counts3_unit['data_UL16_postVFPH'] = {'passJetMult' : 903644, 'passPreSel' : 879317, 'passDiJetMass' : 375509, 'SR' : 142948, 'SB' : 232561, 'passSvB' : 261, 'failSvB' : 569771, }
         
 
         
