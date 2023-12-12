abde037cd688d74ed4584c82c21b3c949f34402e
John Alison <johnda102@gmail.com>
2023-12-12 16:29:36 -0500

    update CI with new plotting

--- .ci-workflows/analysis-plot-job.sh
+++ .ci-workflows/analysis-plot-job.sh
@@ -3,7 +3,7 @@ ls
 echo "############### Moving to python folder"
 cd python/
 echo "############### Running test processor"
-python analysis/makePlots.py --doTest -i hists/test.coffea  -o testCoffeaPlots -m analysis/metadata/plotTest.yml
+python analysis/makePlots.py --doTest -i hists/test.coffea  -o testCoffeaPlots -m analysis/metadata/plotsAll.yml
 echo "############### Checking if pdf files exist"
 ls testCoffeaPlots/RunII/passPreSel/fourTag/SR/SvB_MA_ps_zz.pdf
 ls testCoffeaPlots/RunII/passPreSel/fourTag/SR/SvB_MA_ps_zh.pdf
