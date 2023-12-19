b835b40a61b9ac2167cd7e88d19d0800b5473d08
John Alison <johnda102@gmail.com>
2023-12-18 22:56:48 -0500

    Better cutflow debugging

--- python/analysis/tests/cutflow_test.py
+++ python/analysis/tests/cutflow_test.py
@@ -21,53 +21,57 @@ class CutFlowTestCase(unittest.TestCase):
         self.cf4_unit = hists["cutFlowFourTagUnitWeight"]
         self.cf3      = hists["cutFlowThreeTag"]
         self.cf3_unit = hists["cutFlowThreeTagUnitWeight"]
-
+        self.obs_counts = [self.cf4, self.cf3, self.cf4_unit, self.cf3_unit]
+        
+        
         #  Make these numbers with:
         #  >  python     analysis/tests/dumpCutFlow.py --input [inputFileName] -o [outputFielName]
         #       (python analysis/tests/dumpCutFlow.py --input hists/histAll.coffea -o analysis/tests/histAllCounts.yml )
         #
         knownCountFile = wrapper.args["knownCounts"] 
         self.knownCounts = yaml.safe_load(open(knownCountFile, 'r'))
+        self.cf_names = ["counts4", "counts3", "counts4_unit", "counts3_unit"]
 
+    def get_failures(self, expected, observed):
+        failures = []
+        for datasetAndEra in expected.keys():        
+            for cut, count in expected[datasetAndEra].items():
+                exp = round(float(observed[datasetAndEra][cut]), 2)
+                if abs(count - exp) > 0.1:
+                    failures.append( (datasetAndEra, cut, count, exp) )
+        return failures
         
-    def test_counts4(self):
-        """
-        Test the cutflow for four tag events
-        """
-        for datasetAndEra in self.knownCounts["counts4"].keys():
-            with self.subTest(datasetAndEra=datasetAndEra):
-                for cut, v in self.knownCounts["counts4"][datasetAndEra].items():
-                    self.assertEqual(v,round(float(self.cf4[datasetAndEra][cut]),2),f'incorrect number of fourTag counts for cut: {cut} of dataset {datasetAndEra}')
 
-    def test_counts3(self):
+    def print_test_results(self, failures):
+        print()
+        print()
+        for k, v in failures.items():
+            if len(v):
+                print(f"Failed {k}:")
+                for datasetAndEra, cut, count, exp in v:
+                    percentFail = count/exp if exp else count
+                    print(f"\t{datasetAndEra} {round(percentFail,4)} {cut} {count} {exp}")
+        print()
+        print()
+    
+    def test_counts(self):
         """
-        Test the cutflow for the weighted three tag events
+        Test the cutflow for all events
         """
-        for datasetAndEra in self.knownCounts["counts3"].keys():
-            for cut, v in self.knownCounts["counts3"][datasetAndEra].items():
-                self.assertEqual(v,round(float(self.cf3[datasetAndEra][cut]),2),f'incorrect number of weighted threeTag counts for cut: {cut} of dataset {datasetAndEra}')
+        failures = {}
+        for name, count in zip(self.cf_names, self.obs_counts):
+            failures[name] = self.get_failures(self.knownCounts[name], count)
 
-    def test_counts3_unitWeight(self):
-        """
-        Test the cutflow for the unweighted three tag events
-        """
-        for datasetAndEra in self.knownCounts["counts3_unit"].keys():
-            for cut, v in self.knownCounts["counts3_unit"][datasetAndEra].items():
-                self.assertEqual(v,round(float(self.cf3_unit[datasetAndEra][cut]),2),f'incorrect number of threeTag counts for cut: {cut} of dataset {datasetAndEra}')
-
-    def test_counts4_unitWeight(self):
-        """
-        Test the cutflow for the unweighted fourTag events
-        """
-        for datasetAndEra in self.knownCounts["counts4_unit"].keys():
-            for cut, v in self.knownCounts["counts4_unit"][datasetAndEra].items():
-                self.assertEqual(v,round(float(self.cf4_unit[datasetAndEra][cut]),2),f'incorrect number of fourTag counts for cut: {cut} of dataset {datasetAndEra}')
-
-
-                
+        self.print_test_results(failures)
+            
+        #
+        # Pass/Fail test
+        #
+        for name in self.cf_names:
+            self.assertIs(len(failures[name]),0,f'incorrect number of {name} for {failures[name]} ')
                 
 
 if __name__ == '__main__':
     wrapper.parse_args()
     unittest.main(argv=sys.argv)
-    #unittest.main()
+
