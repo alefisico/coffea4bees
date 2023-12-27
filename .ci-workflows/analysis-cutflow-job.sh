48a794f56dc4e1a3f4ea5466d535f49b4fb18028
algomez <alejandro.gomez@cern.ch>
2023-12-27 13:07:45 -0600

    moving hists and plots to analysis

--- .ci-workflows/analysis-cutflow-job.sh
+++ .ci-workflows/analysis-cutflow-job.sh
@@ -3,6 +3,6 @@ ls
 echo "############### Moving to python folder"
 cd python/
 echo "############### Running cutflow test"
-python analysis/tests/cutflow_test.py   --inputFile hists/test.coffea --knownCounts analysis/tests/testCounts.yml
+python analysis/tests/cutflow_test.py   --inputFile analysis/hists/test.coffea --knownCounts analysis/tests/testCounts.yml
 cd ../
 
