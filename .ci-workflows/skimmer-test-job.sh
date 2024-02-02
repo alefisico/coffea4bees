430efb63bfb611b8153803b379ef4b632e37c6e4
algomez <alejandro.gomez@cern.ch>
2024-02-02 12:49:12 -0600

    ci

--- .ci-workflows/skimmer-test-job.sh
+++ .ci-workflows/skimmer-test-job.sh
@@ -5,7 +5,8 @@ voms-proxy-info
 echo "############### Moving to python folder"
 cd python/
 echo "############### Changing metadata file"
-sed -i 's/base_path.*/base_path: \/builds\/coffea4bees\/python\/skimmer\/rootfiles\//g' skimmer/metadata/HH4b.yml
+sed -i 's/base_path.*/base_path: python\/skimmer\/rootfiles\//g' skimmer/metadata/HH4b.yml
 echo "############### Running test processor"
 python runner.py -s -p skimmer/processor/skimmer_4b.py -m skimmer/metadata/HH4b.yml -y UL18 -d TTTo2L2Nu -op skimmer/metadata/ -o picoaod_datasets.yml -t
+ls -R skimmer/
 cd ../
