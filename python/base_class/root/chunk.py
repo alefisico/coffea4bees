3e43d9a17b9c942d186d9dd93a9a849b4de8f656
ChuyuanLiu <chuyuanliu96@gmail.com>
2024-02-03 13:37:47 -0500

    Add friend tree and modify hist to make it easier to migrate to dask.

--- python/base_class/root/chunk.py
+++ python/base_class/root/chunk.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial, reduce
+from logging import Logger
 from operator import and_
 from typing import Iterable
 from uuid import UUID
@@ -19,7 +20,7 @@ class _ChunkMeta(type):
         return getattr(self, attr)
 
     def __new__(cls, name, bases, dic):
-        for attr in ('branches', 'num_entries', 'entry_stop', 'uuid'):
+        for attr in ('branches', 'num_entries', 'uuid'):
             dic[attr] = property(partial(cls._get, attr=f'_{attr}'))
         return super().__new__(cls, name, bases, dic)
 
@@ -65,10 +66,20 @@ class Chunk(metaclass=_ChunkMeta):
     '''set[str] : Name of branches.'''
     num_entries: int
     '''int : Number of entries.'''
-    entry_start: int
-    '''int : Start entry.'''
-    entry_stop: int
-    '''int : Stop entry.'''
+
+    @property
+    def entry_start(self):
+        '''int : Start entry.'''
+        if self._entry_start is ...:
+            return 0
+        return self._entry_start
+
+    @property
+    def entry_stop(self):
+        '''int : Stop entry.'''
+        if self._entry_stop is ...:
+            return self.num_entries
+        return self._entry_stop
 
     @property
     def offset(self):
@@ -89,12 +100,12 @@ class Chunk(metaclass=_ChunkMeta):
             branches = {*branches}
 
         self.name = name
-        self.entry_start = 0 if entry_start is ... else entry_start
+        self._entry_start = entry_start
+        self._entry_stop = entry_stop
 
         self._uuid = ...
         self._branches = branches
         self._num_entries = num_entries
-        self._entry_stop = num_entries if entry_stop is ... else entry_stop
 
         if check_type(source, PathLike):
             self.path = EOS(source)
@@ -105,29 +116,81 @@ class Chunk(metaclass=_ChunkMeta):
         if fetch:
             self._fetch()
 
-    def _fetch(self, force: bool = False):
-        if force or any(v is ... for v in (self._branches, self._num_entries, self._uuid)):
+    @classmethod
+    def _ignore(cls, value):
+        return value is ... or value is None
+
+    def integrity(
+        self,
+        logger: Logger = None
+    ):
+        """
+        Check and report the following:
+
+        - :data:`path` not exists
+        - :data:`uuid` different from file
+        - :data:`num_entries` different from file
+        - :data:`branches` not in file
+        - :data:`entry_start` out of range
+        - :data:`entry_stop` out of range
+
+        Parameters
+        ----------
+        logger : ~logging.Logger, optional
+            The logger used to report the issues. Can be a :class:`~logging.Logger` or any class with the same interface. If not given, the default logger will be used.
+
+        Returns
+        -------
+        Chunk or None
+            A deep copy of ``self`` with corrected metadata. If file not exists, return ``None``.
+        """
+        if logger is None:
+            logger = Logger.root
+        chunk_name = f'chunk  "{self.path}"\n    '
+        if not self.path.exists:
+            logger.error(f'{chunk_name}file not exists')
+            return None
+        else:
+            reloaded = Chunk(
+                source=self.path,
+                entry_start=self._entry_start,
+                entry_stop=self._entry_stop,
+                fetch=True)
+            if not self._ignore(self._uuid) and self._uuid != reloaded.uuid:
+                logger.error(
+                    f'{chunk_name}UUID {self._uuid}(stored) != {reloaded.uuid}(file)')
+            if not self._ignore(self._num_entries) and self._num_entries != reloaded.num_entries:
+                logger.error(
+                    f'{chunk_name}number of entries {self._num_entries}(stored) != {reloaded.num_entries}(file)')
+            if not self._ignore(self._branches):
+                diff = self._branches - reloaded.branches
+                if diff:
+                    logger.error(
+                        f'{chunk_name}branches {diff} not in file')
+            out_of_range = False
+            if not self._ignore(self._entry_start):
+                out_of_range |= self._entry_start < 0 or self._entry_start >= reloaded.num_entries
+            else:
+                reloaded._entry_start = 0
+            if not self._ignore(self._entry_stop):
+                out_of_range |= self._entry_stop <= reloaded.entry_start or self._entry_stop > reloaded.num_entries
+            else:
+                reloaded._entry_stop = reloaded.num_entries
+            if out_of_range:
+                logger.warning(
+                    f'{chunk_name}invalid entry range [0,{reloaded.num_entries}) -> [{reloaded.entry_start},{reloaded.entry_stop})')
+            return reloaded
+
+    def _fetch(self):
+        if any(v is ... for v in (self._branches, self._num_entries, self._uuid)):
             with uproot.open(self.path) as file:
                 tree = file[self.name]
-
                 if self._branches is ...:
                     self._branches = {*tree.keys()}
-
-                num_entries = tree.num_entries
                 if self._num_entries is ...:
-                    self._num_entries = num_entries
-                    if self._entry_stop is ...:
-                        self._entry_stop = num_entries
-                elif self._num_entries != num_entries:
-                    raise ValueError(
-                        f'The number of entries in the file {num_entries} does not match the stored one {self._num_entries}')
-
-                uuid = file.file.uuid
+                    self._num_entries = tree.num_entries
                 if self._uuid is ...:
-                    self._uuid = uuid
-                elif self._uuid != uuid:
-                    raise ValueError(
-                        f'The UUID of the file {uuid} does not match the stored one {self._uuid}')
+                    self._uuid = file.file.uuid
 
     def __hash__(self):
         return hash((self.uuid, self.name))
@@ -142,29 +205,30 @@ class Chunk(metaclass=_ChunkMeta):
 
     def __repr__(self):
         text = f'TTree:{self.path}'
-        if self._uuid is not ...:
+        if not self._ignore(self._uuid):
             text += f'({self._uuid})'
         text += f':{self.name}'
-        if self._entry_stop is not ...:
-            text += f'[{self.entry_start},{self._entry_stop})'
-        if self._num_entries is not ...:
-            text += f' \u2286 [0,{self._num_entries})'
+        if not self._ignore(self._num_entries):
+            text += f'[0,{self._num_entries})'
+        if not self._ignore(self._entry_start) and not self._ignore(self._entry_stop):
+            text += f' -> [{self._entry_start},{self._entry_stop})'
         return text
 
     def __json__(self):
         json_dict = {
             'path': str(self.path),
-            'uuid': str(self.uuid),
             'name': self.name,
         }
-        if self.branches is not None:
-            json_dict['branches'] = list(self.branches)
-        if self.num_entries is not None:
-            json_dict['num_entries'] = self.num_entries
-        if self.entry_start is not None:
-            json_dict['entry_start'] = self.entry_start
-        if self.entry_stop is not None:
-            json_dict['entry_stop'] = self.entry_stop
+        json_dict['uuid'] = None if self._uuid is ... else str(self._uuid)
+        if self._branches is not None:
+            json_dict['branches'] = None if self._branches is ... else list(
+                self._branches)
+        if self._num_entries is not None:
+            json_dict['num_entries'] = None if self._num_entries is ... else self._num_entries
+        if self._entry_start is not None:
+            json_dict['entry_start'] = None if self._entry_start is ... else self._entry_start
+        if self._entry_stop is not None:
+            json_dict['entry_stop'] = None if self._entry_stop is ... else self._entry_stop
         return json_dict
 
     def deepcopy(self, **kwargs):
@@ -185,7 +249,7 @@ class Chunk(metaclass=_ChunkMeta):
             name=self.name,
             num_entries=self._num_entries,
             branches=kwargs.get('branches', self._branches),
-            entry_start=kwargs.get('entry_start', self.entry_start),
+            entry_start=kwargs.get('entry_start', self._entry_start),
             entry_stop=kwargs.get('entry_stop', self._entry_stop))
 
     def slice(self, start: int, stop: int):
@@ -204,12 +268,6 @@ class Chunk(metaclass=_ChunkMeta):
         """
         start += self.offset
         stop += self.offset
-        invalid = start < self.entry_start or stop < start
-        if self._entry_stop is not ...:
-            invalid |= stop > self._entry_stop
-        if invalid:
-            raise ValueError(
-                f'The slice is not a subset of the chunk [{start},{stop}) \u2284 [{self.entry_start},{self._entry_stop})')
         chunk = self.deepcopy(entry_start=start, entry_stop=stop)
         return chunk
 
@@ -292,13 +350,23 @@ class Chunk(metaclass=_ChunkMeta):
         Chunk
             Chunk from JSON data.
         """
-        return cls(
-            source=(data['path'], UUID(data['uuid'])),
-            name=data['name'],
-            branches=data.get('branches'),
-            num_entries=data.get('num_entries'),
-            entry_start=data.get('entry_start'),
-            entry_stop=data.get('entry_stop'))
+        kwargs = {
+            'name': data['name'],
+        }
+        uuid = data['uuid']
+        if uuid is not None:
+            kwargs['source'] = (data['path'], UUID(uuid))
+        else:
+            kwargs['source'] = data['path']
+        for key in ('branches', 'num_entries', 'entry_start', 'entry_stop'):
+            if key in data:
+                value = data[key]
+                if value is None:
+                    continue
+            else:
+                value = None
+            kwargs[key] = value
+        return cls(**kwargs)
 
     @classmethod
     def from_coffea_processor(cls, events):
