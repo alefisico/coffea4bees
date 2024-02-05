0b1e6bff0c0344bb5f56b7bc163ea612ba71ae74
algomez <alejandro.gomez@cern.ch>
2024-02-05 11:05:54 -0600

    check ci

--- .ci-workflows/skimmer-test-job.sh
+++ .ci-workflows/skimmer-test-job.sh
@@ -5,7 +5,7 @@ voms-proxy-info
 echo "############### Moving to python folder"
 cd python/
 echo "############### Running test processor"
-sed -i 's/2024_v1/test/' skimmer/metadata/HH4b.yml
+sed -i 's?base_path.*?base_path: $CI_PROJECT_DIR?' skimmer/metadata/HH4b.yml
 python runner.py -s -p skimmer/processor/skimmer_4b.py -m skimmer/metadata/HH4b.yml -y UL18 -d TTTo2L2Nu -op skimmer/metadata/ -o picoaod_datasets_TTTo2L2Nu_UL18.yml -t
 ls -R skimmer/
 cd ../
