5adac59620bc6d68d860a3f809ceb2fff72b12ae
algomez <alejandro.gomez@cern.ch>
2024-02-15 13:57:38 -0600

    ci

--- .ci-workflows/skimmer-test-job.sh
+++ .ci-workflows/skimmer-test-job.sh
@@ -5,9 +5,7 @@ voms-proxy-info
 echo "############### Moving to python folder"
 cd python/
 echo "############### Changing metadata"
-sed "s/\#max.*/maxchunks: 5/" skimmer/metadata/HH4b.yml > skimmer/metadata/tmp.yml
-sed "s/\#test.*/test_files: 1/" skimmer/metadata/HH4b.yml > skimmer/metadata/tmp.yml
-sed "s/2024_.*/tmp\//" skimmer/metadata/HH4b.yml > skimmer/metadata/tmp.yml
+sed -e "s/\#max.*/maxchunks: 5/" -e "s/\#test.*/test_files: 1/" -e "s/2024_.*/tmp\//" skimmer/metadata/HH4b.yml > skimmer/metadata/tmp.yml
 cat skimmer/metadata/tmp.yml
 echo "############### Running test processor"
 python runner.py -s -p skimmer/processor/skimmer_4b.py -c skimmer/metadata/tmp.yml -y UL18 -d TTToSemiLeptonic -op skimmer/metadata/ -o picoaod_datasets_TTToSemiLeptonic_UL18.yml -t
