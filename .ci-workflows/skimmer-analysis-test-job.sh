f1b6d1b3fdbaea65a2cacd5b9e25b1a7c6b5e9d6
algomez <alejandro.gomez@cern.ch>
2024-03-05 15:11:22 -0600

    test

--- .ci-workflows/skimmer-analysis-test-job.sh
+++ .ci-workflows/skimmer-analysis-test-job.sh
@@ -4,8 +4,10 @@ echo "############### Checking proxy"
 voms-proxy-info
 echo "############### Moving to python folder"
 cd python/
+echo "############### Modifying previous dataset file"
+sed -i "s/\/builds\/algomez\/coffea4bees\/python\///" skimmer/metadata/picoaod_datasets_TTToSemiLeptonic_UL18.yml
+cat skimmer/metadata/picoaod_datasets_TTToSemiLeptonic_UL18.yml
 echo "############### Modifying dataset file with skimmer ci output"
-sed -i "s/\/builds\/algomez\/coffea4bees\/python//" skimmer/metadata/picoaod_datasets_TTToSemiLeptonic_UL18.yml
 python metadata/merge_yaml_datasets.py -m metadata/datasets_HH4b.yml -f skimmer/metadata/picoaod_datasets_TTToSemiLeptonic_UL18.yml -o metadata/datasets_ci.yml
 cat metadata/datasets_ci.yml
 echo "############### Changing metadata"
--- .ci-workflows/skimmer-test-job.sh
+++ .ci-workflows/skimmer-test-job.sh
@@ -5,9 +5,7 @@ voms-proxy-info
 echo "############### Moving to python folder"
 cd python/
 echo "############### Changing metadata"
-echo "pwd" ${PWD}
-echo "project_path" ${CI_PROJECT_PATH}
-sed -e "s/base_.*/base_path: \/builds\/algomez\/coffea4bees\/python\/skimmer\//" -e "s/\#max.*/maxchunks: 5/" -e "s/\#test.*/test_files: 1/" -e "s/2024_.*/tmp\//" -e "s/T3_US_FNALLPC/T3_CH_PSI/" skimmer/metadata/HH4b.yml > skimmer/metadata/tmp.yml
+sed -e "s/base_.*/base_path: \/builds\/${CI_PROJECT_PATH}\/python\/skimmer\//" -e "s/\#max.*/maxchunks: 5/" -e "s/\#test.*/test_files: 1/" -e "s/2024_.*/tmp\//" -e "s/T3_US_FNALLPC/T3_CH_PSI/" skimmer/metadata/HH4b.yml > skimmer/metadata/tmp.yml
 cat skimmer/metadata/tmp.yml
 echo "############### Running test processor"
 python runner.py -s -p skimmer/processor/skimmer_4b.py -c skimmer/metadata/tmp.yml -y UL18 -d TTToSemiLeptonic -op skimmer/metadata/ -o picoaod_datasets_TTToSemiLeptonic_UL18.yml -t
