0d3bb5b5cf069467f2b22e731dfa1d57904acbaf
John Alison <johnda102@gmail.com>
2024-03-12 11:06:05 -0400

    Update Code

--- .ci-workflows/analysis-makeweights-job.sh
+++ .ci-workflows/analysis-makeweights-job.sh
@@ -4,7 +4,7 @@ echo "############### Moving to python folder"
 cd python/
 echo "############### Running makeweights test"
 python analysis/make_weights.py -o testJCM_ROOT   -c passPreSel -r SB --ROOTInputs --i analysis/tests/HistsFromROOTFile.coffea
-python analysis/make_weights.py -o testJCM_Coffea -c passPreSel -r SB -i analysis/hists/test.coffea  -m analysis/metadata/plotsJCM.yml
+python analysis/make_weights.py -o testJCM_Coffea -c passPreSel -r SB -i analysis/hists/test.coffea
 python analysis/tests/make_weights_test.py 
 cd ../
 
