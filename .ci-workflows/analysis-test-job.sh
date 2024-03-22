098d938201f9a8897e40e8febb45187e4eff7d61
Alejandro <alejandro.gomez@cern.ch>
2024-03-22 16:10:11 +0100

    fixing ci analysis-test

--- .ci-workflows/analysis-test-job.sh
+++ .ci-workflows/analysis-test-job.sh
@@ -4,6 +4,9 @@ echo "############### Checking proxy"
 voms-proxy-info
 echo "############### Moving to python folder"
 cd python/
+echo "############### Modifying config"
+sed -e "s/condor_cores.*/condor_cores: 6/" -e "s/condor_memory.*/condor_memory: 8GB/" -i analysis/metadata/HH4b.yml
+cat analysis/metadata/HH4b.yml
 echo "############### Running test processor"
 python runner.py -t -o test.coffea -d data TTToHadronic TTToSemiLeptonic TTTo2L2Nu  HH4b ZH4b ZZ4b -p analysis/processors/processor_HH4b.py -y UL17 UL18 UL16_preVFP UL16_postVFP -op analysis/hists/
 ls
