5a09094809b32ff3a2a31c52ab3303471ec59020
ChuyuanLiu <chuyuanliu96@gmail.com>
2024-04-10 23:02:40 -0400

    add a profiler for test

---
+++ python/classifier/config/main/profiler.py
@@ -0,0 +1,74 @@
+import logging
+
+from classifier.task import ArgParser, EntryPoint, Model, parse
+
+from .. import setting as cfg
+from ._utils import LoadTrainingSets, SelectDevice
+from .help import _print_mod
+
+_PROFILE_DEFAULT_OPTS = {
+    "record_shapes": True,
+    "profile_memory": True,
+    "with_stack": True,
+    "with_flops": True,
+    "with_modules": True,
+}
+
+
+class Main(SelectDevice, LoadTrainingSets):
+    argparser = ArgParser(
+        prog="profiler",
+        description="""Run torch profiler on models.
+[1] https://pytorch.org/blog/understanding-gpu-memory-1/
+[2] https://pytorch.org/blog/understanding-gpu-memory-2/""",
+        workflow=[
+            *LoadTrainingSets._workflow,
+            ("main", "call [blue]model.profile()[/blue]"),
+            ("main", "train [blue]model[/blue] with profiler enabled"),
+        ],
+    )
+    argparser.add_argument(
+        "--profile-options",
+        type=parse.mapping,
+        default="",
+        help=f"profiling options {parse.EMBED}",
+    )
+    argparser.add_argument(
+        "--profile-schedule",
+        type=parse.mapping,
+        default="",
+        help=f"profiling schedule {parse.EMBED}",
+    )
+    argparser.add_argument(
+        "--profile-activities",
+        nargs="+",
+        default=["CPU", "CUDA"],
+        help="profiling activities",
+    )
+
+    def run(self, parser: EntryPoint):
+        from torch.profiler import ProfilerActivity, profile, schedule
+
+        # load datasets in parallel
+        datasets = self.load_training_sets(parser)
+        # initialize datasets
+        m_initializer: list[Model] = parser.mods["model"]
+        args = parser.args["model"]
+        results = {}
+        # train models in sequence
+        for i, model in enumerate(m_initializer):
+            logging.info(f"Initalizing {_print_mod('model', *args[i], ' ')}")
+            trainers = model.train()
+            for j, trainer in enumerate(trainers):
+                name = f"{i}.{j}"
+                with profile(
+                    activities=[
+                        getattr(ProfilerActivity, a)
+                        for a in self.opts.profile_activities
+                    ],
+                    schedule=schedule(**self.opts.profile_schedule),
+                    **(_PROFILE_DEFAULT_OPTS | self.opts.profile_options),
+                ) as p:
+                    results[name] = trainer(self.device, datasets)
+                p.export_memory_timeline(cfg.IO.profiler / f"profiler_{name}.html")
+        return {"models": results}
