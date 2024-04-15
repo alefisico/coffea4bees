ce08ab2c16f0641a50b37282b90664d36d1c3110
ChuyuanLiu <chuyuanliu96@gmail.com>
2024-04-15 15:20:22 -0400

    allow to log renderables

---
+++ python/classifier/monitor/logging/__init__.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+import logging
+
+from ...config.setting import Monitor as cfg
+from ...process.monitor import Recorder
+from ._redirect import MultiPlatformHandler
+
+
+def _disable_logging():
+    logging.basicConfig(handlers=[logging.NullHandler()], level=None)
+
+
+def setup_reporter():
+    if cfg.log_enable:
+        return logging.basicConfig(
+            handlers=[MultiPlatformHandler(level=cfg.logging_level)],
+            level=cfg.logging_level,
+        )
+    _disable_logging()
+
+
+def setup_monitor():
+    if cfg.log_enable:
+        handlers = []
+        if cfg.console_enable:
+            from ..backends.console import Dashboard as _CD
+            from ._console import ConsoleDump, ConsoleHandler
+
+            ConsoleDump.init()
+            Recorder.to_dump(cfg.file_log, ConsoleDump.serialize)
+            handlers.append(ConsoleDump.handler)
+            handlers.append(ConsoleHandler(markup=True, console=_CD.console))
+        if handlers:
+            return logging.basicConfig(
+                handlers=[
+                    MultiPlatformHandler.init(
+                        level=cfg.logging_level, handlers=handlers
+                    )
+                ],
+                level=cfg.logging_level,
+            )
+    _disable_logging()
