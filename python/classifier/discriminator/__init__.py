1cac5711c7fcbfa5d21f429cd9e1759986493843
ChuyuanLiu <chuyuanl@andrew.cmu.edu>
2024-04-17 16:03:51 -0500

    cleanup

--- python/classifier/discriminator/__init__.py
+++ python/classifier/discriminator/__init__.py
@@ -139,14 +139,12 @@ class Classifier(WithUUID, ABC):
                 loss.backward()
                 optimizer.step()
                 batch_p.update(i + 1, f"batch|loss={loss.item():.4g}")
-            batch_p.complete()
             if stage.do_benchmark:
                 ...  # TODO benchmark
             lr.step()
             bs.step()
             model.step()
             epoch_p.update(epoch + 1)
-        epoch_p.complete()
         return benchmark
 
     @torch.no_grad()
