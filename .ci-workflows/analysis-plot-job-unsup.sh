b858bcba39ec08355cc16552d2b2349d6786be9d
Sindhu Murthy <sindhum@andrew.cmu.edu>
2024-04-18 14:14:56 -0500

    unsup processor into CI

--- .ci-workflows/analysis-plot-job-unsup.sh
+++ .ci-workflows/analysis-plot-job-unsup.sh
@@ -5,14 +5,9 @@ cd python/
 echo "############### Running test processor"
 python analysis/makePlots.py analysis/hists/test_unsup.coffea --doTest   -o analysis/testCoffeaPlots -m analysis/metadata/plotsAll_unsup.yml
 echo "############### Checking if pdf files exist"
-ls analysis/testCoffeaPlots/RunII/passPreSel/fourTag/SR/SvB_MA_ps_zz.pdf
-ls analysis/testCoffeaPlots/RunII/passPreSel/fourTag/SR/SvB_MA_ps_zh.pdf
-ls analysis/testCoffeaPlots/RunII/passPreSel/fourTag/SR/SvB_MA_ps_hh.pdf
-ls analysis/testCoffeaPlots/RunII/passPreSel/fourTag/SR_vs_SB/data/SvB_MA_ps.pdf
-ls analysis/testCoffeaPlots/RunII/passPreSel/fourTag/SR_vs_SB/HH4b/SvB_MA_ps.pdf
-ls analysis/testCoffeaPlots/RunII/passPreSel_vs_failSvB_vs_passSvB/fourTag/SR/data/v4j_mass.pdf
-ls analysis/testCoffeaPlots/RunII/passPreSel_vs_failSvB_vs_passSvB/fourTag/SR/HH4b/v4j_mass.pdf 
-ls analysis/testCoffeaPlots/RunII/passPreSel/fourTag/SR/data/quadJet_min_dr_close_vs_other_m.pdf 
-ls analysis/testCoffeaPlots/RunII/passPreSel/fourTag/SR/HH4b/quadJet_min_dr_close_vs_other_m.pdf
-ls analysis/testCoffeaPlots/RunII/passPreSel/threeTag/SR/Multijet/quadJet_min_dr_close_vs_other_m.pdf 
+ls analysis/testCoffeaPlots/RunII/passPreSel/fourTag/SR/v4j_mass.pdf
+ls analysis/testCoffeaPlots/RunII/passPreSel/fourTag/SR_vs_SB/mixeddata/v4j_mass.pdf
+ls analysis/testCoffeaPlots/RunII/passPreSel/fourTag/SR_vs_SB/data_3b_for_mixed/v4j_mass.pdf
+ls analysis/testCoffeaPlots/RunII/passPreSel/fourTag/SR/mixeddata/quadJet_selected_lead_vs_subl_m.pdf 
+ls analysis/testCoffeaPlots/RunII/passPreSel/threeTag/SR/Multijet/quadJet_selected_lead_vs_subl_m.pdf 
 cd ../
