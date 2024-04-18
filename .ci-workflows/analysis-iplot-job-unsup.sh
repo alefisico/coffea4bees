e6c8d133b8b54a60547a1adcf35cce054e0004d1
Sindhu Murthy <sindhum@andrew.cmu.edu>
2024-04-18 14:14:12 -0500

    unsup processor into CI

---
+++ .ci-workflows/analysis-iplot-job-unsup.sh
@@ -0,0 +1,6 @@
+echo "############### Checking ls"
+ls
+echo "############### Moving to python folder"
+cd python/
+echo "############### Running iPlot test"
+python analysis/tests/iPlot_test.py --inputFile analysis/hists/test_unsup.coffea
---
+++ .ci-workflows/analysis-test-job-unsup.sh
@@ -0,0 +1,13 @@
+echo "############### Including proxy"
+export X509_USER_PROXY=${PWD}/proxy/x509_proxy
+echo "############### Checking proxy"
+voms-proxy-info
+echo "############### Moving to python folder"
+cd python/
+echo "############### Modifying config"
+sed -e "s/condor_cores.*/condor_cores: 6/" -e "s/condor_memory.*/condor_memory: 8GB/" -i analysis/metadata/HH4b.yml
+cat analysis/metadata/unsup4b.yml
+echo "############### Running test processor"
+python runner.py -t -o test_unsup.coffea -d mixeddata data_3b_for_mixed TTToHadronic TTToSemiLeptonic TTTo2L2Nu -p analysis/processors/processor_unsup.py -y UL17 UL18 UL16_preVFP UL16_postVFP -op analysis/hists/ -m analysis/metadata/unsup4b.yml -c analysis/metadata/unsup4b.yml
+ls
+cd ../
