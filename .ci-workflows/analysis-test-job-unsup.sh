cd5ccd4aeb659915502ea162e6ce50f2546a0f20
Sindhu Murthy <sindhum@andrew.cmu.edu>
2024-04-23 15:33:56 -0500

    ci workflow script

--- .ci-workflows/analysis-test-job-unsup.sh
+++ .ci-workflows/analysis-test-job-unsup.sh
@@ -5,7 +5,7 @@ voms-proxy-info
 echo "############### Moving to python folder"
 cd python/
 echo "############### Modifying config"
-sed -e "s/condor_cores.*/condor_cores: 6/" -e "s/condor_memory.*/condor_memory: 8GB/" -i analysis/metadata/unsup.yml
+sed -e "s/condor_cores.*/condor_cores: 6/" -e "s/condor_memory.*/condor_memory: 8GB/" -i analysis/metadata/unsup4b.yml
 cat analysis/metadata/unsup4b.yml
 echo "############### Running test processor"
 python runner.py -t -o test_unsup.coffea -d mixeddata data_3b_for_mixed TTToHadronic TTToSemiLeptonic TTTo2L2Nu -p analysis/processors/processor_unsup.py -y UL17 UL18 UL16_preVFP UL16_postVFP -op analysis/hists/ -m analysis/metadata/unsup4b.yml -c analysis/metadata/unsup4b.yml
