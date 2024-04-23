708898ed8431581035b7a8aa4220d2f11058f783
Sindhu Murthy <sindhum@andrew.cmu.edu>
2024-04-23 15:29:01 -0500

    ci plot workflow for unsup

--- .ci-workflows/analysis-iplot-job-unsup.sh
+++
@@ -1,8 +0,0 @@
-echo "############### Checking ls"
-ls
-echo "############### Moving to python folder"
-cd python/
-echo "############### Running iPlot test"
-python analysis/tests/iPlot_test.py --inputFile analysis/hists/test_unsup.coffea
-
-
