ed0367f3c3aac4276f566123300a773bdf77c6c2
Sindhu Murthy <sindhum@andrew.cmu.edu>
2024-04-24 16:24:45 -0500

    unsup ci merge

--- .ci-workflows/analysis-cutflow-job-unsup.sh
+++ .ci-workflows/analysis-cutflow-job-unsup.sh
@@ -4,7 +4,8 @@ echo "############### Moving to python folder"
 cd python/
 echo "############### Running cutflow test"
 python analysis/tests/cutflow_test.py   --inputFile analysis/hists/test_unsup.coffea --knownCounts analysis/tests/testCounts_unsup.yml
-echo "############### Running dump cutflow test"
+echo "############### Runpwd
+ning dump cutflow test"
 python analysis/tests/dumpCutFlow.py --input analysis/hists/test_unsup.coffea -o analysis/tests/test_dump_cutflow_unsup.yml
 ls analysis/tests/test_dump_cutflow_unsup.yml
 cd ../
