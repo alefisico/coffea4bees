cecbfd3fcc181aa66dc5ec9f50af0caf6631e826
Sindhu Murthy <sindhum@andrew.cmu.edu>
2024-04-23 20:12:54 -0500

    ci plot workflow for unsup

--- .ci-workflows/analysis-plot-job-unsup.sh
+++ .ci-workflows/analysis-plot-job-unsup.sh
@@ -5,7 +5,7 @@ cd python/
 echo "############### Running test processor"
 python analysis/makePlots_unsup.py analysis/hists/test_unsup.coffea --doTest   -o analysis/testCoffeaPlots_unsup/ -m analysis/metadata/plotsAll_unsup.yml 
 echo "############### Checking if pdf files exist"
-ls analysis/testCoffeaPlots_unsup/RunII/passPreSel/fourTag/SR/v4j_mass.pdf
+ls analysis/testCoffeaPlots_unsup/RunII/passPreSel/fourTag/SR/mix_v0/v4j_mass.pdf
 ls analysis/testCoffeaPlots_unsup/RunII/passPreSel/fourTag/SR_vs_SB/mix_v0/v4j_mass.pdf
 ls analysis/testCoffeaPlots_unsup/RunII/passPreSel/fourTag/SR/mix_v0/quadJet_selected_lead_vs_subl_m.pdf 
 ls analysis/testCoffeaPlots_unsup/RunII/passPreSel/threeTag/SR/data_3b_for_mixed/quadJet_selected_lead_vs_subl_m.pdf 
