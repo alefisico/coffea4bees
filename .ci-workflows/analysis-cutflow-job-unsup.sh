73580cf28ac95d24d1f499978e4b6a82ba9a3231
Sindhu Murthy <sindhum@andrew.cmu.edu>
2024-04-25 15:05:25 -0500

    cutflow ci fix

--- .ci-workflows/analysis-cutflow-job-unsup.sh
+++ .ci-workflows/analysis-cutflow-job-unsup.sh
@@ -4,9 +4,7 @@ echo "############### Moving to python folder"
 cd python/
 echo "############### Running cutflow test"
 python analysis/tests/cutflow_test.py   --inputFile analysis/hists/test_unsup.coffea --knownCounts analysis/tests/testCounts_unsup.yml
-echo "############### Runpwd
-ning dump cutflow test"
+echo "############### Running dump cutflow test"
 python analysis/tests/dumpCutFlow.py --input analysis/hists/test_unsup.coffea -o analysis/tests/test_dump_cutflow_unsup.yml
 ls analysis/tests/test_dump_cutflow_unsup.yml
 cd ../
-
