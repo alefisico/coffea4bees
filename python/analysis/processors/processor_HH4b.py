72d7011ac64d45b21dfa683ad16b148b520f34f4
algomez <alejandro.gomez@cern.ch>
2024-05-01 15:49:46 -0500

    generalizing SvB files

--- python/analysis/processors/processor_HH4b.py
+++ python/analysis/processors/processor_HH4b.py
@@ -199,13 +199,15 @@ class analysis(processor.ProcessorABC):
 
         if self.run_SvB:
             if (self.classifier_SvB is None) | (self.classifier_SvB_MA is None):
-                event["SvB"] = ( NanoEventsFactory.from_root( f'{path}{"SvB_newSBDef.root" if "mix" in dataset else "SvB.root"}',
+                SvB_file = f'{path}/SvB_newSBDef.root' if 'mix' in dataset else f'{fname.replace("picoAOD", "SvB")}'
+                event["SvB"] = ( NanoEventsFactory.from_root( SvB_file,
                                                               entry_start=estart, entry_stop=estop, schemaclass=FriendTreeSchema).events().SvB )
 
                 if not ak.all(event.SvB.event == event.event):
                     raise ValueError("ERROR: SvB events do not match events ttree")
 
-                event["SvB_MA"] = ( NanoEventsFactory.from_root( f'{path}{"SvB_MA_newSBDef.root" if "mix" in dataset else "SvB_MA.root"}',
+                SvB_MA_file = f'{path}/SvB_MA_newSBDef.root' if 'mix' in dataset else f'{fname.replace("picoAOD", "SvB_MA")}'
+                event["SvB_MA"] = ( NanoEventsFactory.from_root( SvB_MA_file,
                                                                  entry_start=estart, entry_stop=estop, schemaclass=FriendTreeSchema ).events().SvB_MA )
 
                 if not ak.all(event.SvB_MA.event == event.event):
