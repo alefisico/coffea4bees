Flag for Lep remove, add underflow overflow in conversion, plus 1 bug

python/analysis/helpers/selection_basic_4b.py
@@ -22,7 +22,7 @@ def apply_event_selection_4b( event, isMC, corrections_metadata, isMixedData = F
 
     return event
 
-def apply_object_selection_4b( event, year, isMC, dataset, corrections_metadata, *, isMixedData=False, isTTForMixed=False, isDataForMixed=False  ):
+def apply_object_selection_4b( event, year, isMC, dataset, corrections_metadata, *, isMixedData=False, isTTForMixed=False, isDataForMixed=False, doLeptonRemoval=True  ):
     """docstring for apply_basic_selection_4b. This fuction is not modifying the content of anything in events. it is just adding it"""
 
     #
@@ -53,7 +53,10 @@ def apply_object_selection_4b( event, year, isMC, dataset, corrections_metadata,
     else:
 
         event['Jet', 'calibration'] = event.Jet.pt / ( event.Jet.pt_raw if 'pt_raw' in event.Jet.fields else ak.full_like(event.Jet.pt, 1) )
-        event['Jet', 'lepton_cleaned'] = drClean( event.Jet, selLepton )[1]  ### 0 is the collection of jets, 1 is the flag
+        if doLeptonRemoval:
+            event['Jet', 'lepton_cleaned'] = drClean( event.Jet, selLepton )[1]  ### 0 is the collection of jets, 1 is the flag
+        else:
+            event['Jet', 'lepton_cleaned'] = np.full(len(event), True)
 
         event['Jet', 'pileup'] = ((event.Jet.puId < 0b110) & (event.Jet.pt < 50)) | ((np.abs(event.Jet.eta) > 2.4) & (event.Jet.pt < 40))
         event['Jet', 'selected_loose'] = (event.Jet.pt >= 20) & ~event.Jet.pileup & (event.Jet.jetId>=2) & event.Jet.lepton_cleaned

python/stats_analysis/convert_hist_to_json.py
@@ -4,7 +4,7 @@ import logging
 import json
 import numpy as np
 from coffea.util import load
-
+import hist
 
 def hist_to_json( coffea_hist ):
     """docstring for hist_to_root"""
@@ -14,6 +14,10 @@ def hist_to_json( coffea_hist ):
         'centers' : coffea_hist.axes[0].centers.tolist(),
         'values' : coffea_hist.values().tolist(),
         'variances' : coffea_hist.variances().tolist(),
+        'underflow_value' : coffea_hist[hist.loc(-np.inf)].value,
+        'underflow_variance' : coffea_hist[hist.loc(-np.inf)].variance,
+        'overflow_value' : coffea_hist[hist.loc(+np.inf)].value,
+        'overflow_variance' : coffea_hist[hist.loc(+np.inf)].variance,
     }
 
 