98210414befff7af47ffe3029efa4e2498d89a01
Sindhu Murthy <sindhum@andrew.cmu.edu>
2024-05-07 12:42:51 -0500

    revert ci analysis test

--- .ci-workflows/analysis-test-job-unsup.sh
+++ .ci-workflows/analysis-test-job-unsup.sh
@@ -4,13 +4,10 @@ echo "############### Checking proxy"
 voms-proxy-info
 echo "############### Moving to python folder"
 cd python/
-if [[ $(hostname) = *fnal* ]]; then
-    DATASETS=analysis/metadata/unsup4b.yml
-else
-    DATASETS=metadata/datasets_HH4b_cernbox.yml
-fi
-echo "############### Running datasets from " $DATASETS
-echo "############### Running unsup test processor"
-python runner.py -t -o test_unsup.coffea -d mixeddata data_3b_for_mixed TTToHadronic TTToSemiLeptonic TTTo2L2Nu -p analysis/processors/processor_unsup.py -y UL17 UL18 UL16_preVFP UL16_postVFP -op analysis/hists/ -c analysis/metadata/unsup4b.yml -m $DATASETS
+echo "############### Modifying config"
+sed -e "s/condor_cores.*/condor_cores: 6/" -e "s/condor_memory.*/condor_memory: 8GB/" -i analysis/metadata/unsup4b.yml
+cat analysis/metadata/unsup4b.yml
+echo "############### Running test processor"
+python runner.py -t -o test_unsup.coffea -d mixeddata data_3b_for_mixed TTToHadronic TTToSemiLeptonic TTTo2L2Nu -p analysis/processors/processor_unsup.py -y UL17 UL18 UL16_preVFP UL16_postVFP -op analysis/hists/ -m analysis/metadata/unsup4b.yml -c analysis/metadata/unsup4b.yml
 ls
 cd ../
