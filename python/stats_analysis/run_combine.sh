4f0d25c389b7172eb6958b98b69b25e384342392
algomez <alejandro.gomez@cern.ch>
2024-05-09 15:18:06 -0500

    add genmatch and make combine cards with syst

--- python/stats_analysis/run_combine.sh
+++ python/stats_analysis/run_combine.sh
@@ -1,4 +1,7 @@
-combine_inputs="datacards_HH_oldbkg_woZ/combine_SvB"
+datacard_folder=$1
 
-text2workspace.py ${combine_inputs}.txt -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose --PO 'map=.*/HH:rHH[1,-10,10]' --PO 'map=.*/ZH:rZH[1,-10,10]' --PO 'map=.*/ZZ:rZZ[1,-10,10]'
-combine -M AsymptoticLimits ${combine_inputs}.root --redefineSignalPOIs rHH
+text2workspace.py ${datacard_folder}/combine_SvB.txt -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose --PO 'map=.*/HH:rHH[1,-10,10]' --PO 'map=.*/ZH:rZH[1,-10,10]' --PO 'map=.*/ZZ:rZZ[1,-10,10]'
+combine -M AsymptoticLimits ${datacard_folder}/combine_SvB.root --redefineSignalPOIs rHH
+
+text2workspace.py ${datacard_folder}/combine_SvB_MA.txt -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose --PO 'map=.*/HH:rHH[1,-10,10]' --PO 'map=.*/ZH:rZH[1,-10,10]' --PO 'map=.*/ZZ:rZZ[1,-10,10]'
+combine -M AsymptoticLimits ${datacard_folder}/combine_SvB_MA.root --redefineSignalPOIs rHH
