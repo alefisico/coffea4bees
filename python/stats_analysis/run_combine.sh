1681a377ffff4bb441ee330009626aac8c242919
algomez <alejandro.gomez@cern.ch>
2024-05-19 10:05:50 -0500

    updating combine scripts

--- python/stats_analysis/run_combine.sh
+++ python/stats_analysis/run_combine.sh
@@ -1,7 +1,20 @@
 datacard_folder=$1
 
-text2workspace.py ${datacard_folder}/combine_SvB.txt -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose --PO 'map=.*/HH:rHH[1,-10,10]' --PO 'map=.*/ZH:rZH[1,-10,10]' --PO 'map=.*/ZZ:rZZ[1,-10,10]'
-combine -M AsymptoticLimits ${datacard_folder}/combine_SvB.root --redefineSignalPOIs rHH
+for iclass in SvB_MA;
+do
+    text2workspace.py ${datacard_folder}/combine_${iclass}.txt -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose --PO 'map=.*/HH:rHH[1,-10,10]' #--PO 'map=.*/ZH:rZH[1,-10,10]' --PO 'map=.*/ZZ:rZZ[1,-10,10]'
+    combine -M AsymptoticLimits ${datacard_folder}/combine_${iclass}.root --redefineSignalPOIs rHH -n _${iclass} > ${datacard_folder}/limits.txt
 
-text2workspace.py ${datacard_folder}/combine_SvB_MA.txt -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO verbose --PO 'map=.*/HH:rHH[1,-10,10]' --PO 'map=.*/ZH:rZH[1,-10,10]' --PO 'map=.*/ZZ:rZZ[1,-10,10]'
-combine -M AsymptoticLimits ${datacard_folder}/combine_SvB_MA.root --redefineSignalPOIs rHH
+    if [ $# -eq 2 ]
+      then
+        combineTool.py -M Impacts -d ${datacard_folder}/combine_${iclass}.root --doInitialFit --setParameterRanges rHH=-10,10 --setParameters rHH=1 --robustFit 1 -m 125 -n ${iclass} -t -1 ## expected -t -1
+
+        combineTool.py -M Impacts -d ${datacard_folder}/combine_${iclass}.root --doFits --setParameterRanges rHH=-10,10 --setParameters rHH=1 --robustFit 1 -m 125 --parallel 4 -n ${iclass} -t -1
+
+        combineTool.py -M Impacts -d ${datacard_folder}/combine_${iclass}.root -o ${datacard_folder}/impacts_combine_${iclass}_exp.json -m 125 -n ${iclass}
+
+        plotImpacts.py -i ${datacard_folder}/impacts_combine_${iclass}_exp.json -o ${datacard_folder}/impacts_combine_${iclass}_exp_HH --POI rHH --per-page 20 --left-margin 0.3 --height 400 --label-size 0.04 --translate nuisance_names.json
+
+    fi
+    mv higgsCombine_*  ${datacard_folder}/
+done
