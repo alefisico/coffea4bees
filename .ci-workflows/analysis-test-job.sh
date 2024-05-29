030aba8da9dede9a1dd15c5e4b975741d8913340
John Alison <johnalison@cmu.edu>
2024-05-28 21:29:46 -0500

    FvT in fix datasets

--- .ci-workflows/analysis-test-job.sh
+++ .ci-workflows/analysis-test-job.sh
@@ -12,6 +12,7 @@ else
     #sed -e "s/condor_cores.*/condor_cores: 6/" -e "s/condor_memory.*/condor_memory: 8GB/" -i analysis/metadata/HH4b.yml
     #cat analysis/metadata/HH4b.yml
 fi
+DATASETS=metadata/datasets_HH4b_cernbox.yml
 echo "############### Running datasets from " $DATASETS
 echo "############### Running test processor"
 python runner.py -t -o test.coffea -d data TTToHadronic TTToSemiLeptonic TTTo2L2Nu  ZH4b ZZ4b GluGluToHHTo4B_cHHH1 -p analysis/processors/processor_HH4b.py -y UL17 UL18 UL16_preVFP UL16_postVFP -op analysis/hists/ -m $DATASETS
