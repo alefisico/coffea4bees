dd4572585708de14467468c4c0b7a83f1ecc8a7e
ChuyuanLiu <chuyuanliu96@gmail.com>
2024-06-19 00:46:22 -0400

    allow to modify output picoAOD in skimmer, add a test case

--- python/skimmer/processor/picoaod.py
+++ python/skimmer/processor/picoaod.py
@@ -1,26 +1,52 @@
+from __future__ import annotations
+
+import gc
 import logging
 import re
 from abc import abstractmethod
 from concurrent.futures import Future, ProcessPoolExecutor
+from itertools import chain
+from typing import Iterable
 
 import awkward as ak
 import numpy as np
 import numpy.typing as npt
 import uproot
+from analysis.helpers.cutflow import cutFlow
 from base_class.awkward.zip import NanoAOD
 from base_class.dask.delayed import delayed
 from base_class.root import Chunk, TreeReader, TreeWriter, merge
 from base_class.system.eos import EOS, PathLike
 from base_class.utils.wrapper import retry
 from coffea.processor import ProcessorABC
-from analysis.helpers.cutflow import cutFlow
 
 _PICOAOD = "picoAOD"
 _ROOT = ".root"
 
 
-def _return_empty(*_):
+class SkimmingError(Exception):
+    __module__ = Exception.__module__
+
+
+def _log_exception(e, *_):
+    logging.error(f"The following exception occurred during skimming:", exc_info=e)
     return {}
+
+
+def _clear_cache(events: ak.Array):
+    # try to clear cached branches
+    for cache in events.caches:
+        cache.clear()
+    gc.collect()
+
+
+def _branch_filter(collections: Iterable[str], branches: Iterable[str]):
+    branches = chain(
+        map("({}_.*)".format, collections or ()),
+        map("(n{})".format, collections or ()),
+        map("({})".format, branches or ()),
+    )
+    return rf'^(?!({"|".join(branches)})$).*$'
 
 
 class PicoAOD(ProcessorABC):
@@ -28,42 +54,47 @@ class PicoAOD(ProcessorABC):
         self,
         base_path: PathLike,
         step: int,
-        skip_collections: list[str] = None,
-        skip_branches: list[str] = None,
+        skip_collections: Iterable[str] = None,
+        skip_branches: Iterable[str] = None,
     ):
         self._base = EOS(base_path)
         self._step = step
-        if skip_collections is None:
-            skip_collections = []
-        if skip_branches is None:
-            skip_branches = []
-        skipped = (
-            [f"{collection}_.*" for collection in skip_collections]
-            + [f"n{collection}" for collection in skip_collections]
-            + skip_branches
+        self._branch_filter = re.compile(
+            _branch_filter(skip_collections, skip_branches)
         )
-        self._filter_branches = re.compile(f'^(?!({"|".join(skipped)})$).*$')
         self._transform = NanoAOD(regular=False, jagged=True)
         self.cutFlowCuts = []
 
     def _filter(self, branches: set[str]):
-        return {*filter(self._filter_branches.match, branches)}
+        return {*filter(self._branch_filter.match, branches)}
 
     @abstractmethod
     def select(
         self, events: ak.Array
-    ) -> npt.ArrayLike | tuple[npt.ArrayLike, ak.Array | None, dict | None]:
+    ) -> (
+        npt.NDArray[np.bool_]
+        | tuple[npt.NDArray[np.bool_], ak.Array]
+        | tuple[npt.NDArray[np.bool_], ak.Array | None, dict]
+    ):
         pass
 
     # no retry, return empty dict if any exception
-    @retry(1, handler=_return_empty)
+    @retry(1, handler=_log_exception, skip=(SkimmingError,))
     def process(self, events: ak.Array):
         EOS.set_retry(3, 10)  # 3 retries with 10 seconds interval
         self._cutFlow = cutFlow(self.cutFlowCuts)
         selected = self.select(events)
+        added, result = None, {}
+        if isinstance(selected, tuple):
+            if len(selected) >= 2:
+                added = selected[1]
+            if len(selected) >= 3:
+                result = selected[2] or result
+            selected = selected[0]
         chunk = Chunk.from_coffea_events(events)
         dataset = events.metadata["dataset"]
 
+        # construct output
         result = {
             dataset: {
                 "total_events": len(events),
@@ -71,18 +102,29 @@ class PicoAOD(ProcessorABC):
                 "files": [],
                 "source": {str(chunk.path): [(chunk.entry_start, chunk.entry_stop)]},
             }
+            | result
         }
-
         lumi = events.metadata.get("lumi", 1.0)
         xs = events.metadata.get("xs", 1.0)
         kFactor = events.metadata.get("kFactor", 1.0)
         self._cutFlow.addOutputSkim(result, dataset, lumi=lumi, xs=xs, kFactor=kFactor)
 
+        # sanity check
+        if (
+            added is not None
+            and (size := len(added)) != result[dataset]["saved_events"]
+        ):
+            raise SkimmingError(
+                f"Length of additional branches ({size}) does not match the number of selected events ({result[dataset]['saved_events']})"
+            )
+        # clear cache
+        _clear_cache(events)
+        # save selected events
         if result[dataset]["saved_events"] > 0:
             filename = f"{dataset}/{_PICOAOD}_{chunk.uuid}_{chunk.entry_start}_{chunk.entry_stop}{_ROOT}"
             path = self._base / filename
-            reader = TreeReader(self._filter, self._transform)
-
+            reader = TreeReader(self._filter)
+            saved = 0
             with TreeWriter()(path) as writer:
                 for i, chunks in enumerate(
                     Chunk.partition(self._step, chunk, common_branches=True)
@@ -94,11 +136,17 @@ class PicoAOD(ProcessorABC):
                     _start, _stop = _range[0], _range[-1] + 1
                     _chunk = chunks[0].slice(_start, _stop)
                     _selected = _selected[_start:_stop]
-                    data = reader.arrays(_chunk)
-                    writer.extend(data[_selected])
+                    data = reader.arrays(_chunk)[_selected]
+                    if added is not None:
+                        _saved = saved + ak.sum(_selected)
+                        _added = added[saved:_saved]
+                        for k in added.fields:
+                            data[k] = _added[k]
+                        saved = _saved
+                    data = self._transform(data)
+                    writer.extend(data)
             if writer.tree is not None:
                 result[dataset]["files"].append(writer.tree)
-
         return result
 
     def postprocess(self, accumulator):
