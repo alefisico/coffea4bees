7085b42a93cfcdef1980110c1d07cecf4a66b478
John Alison <johnalison@cmu.edu>
2024-08-07 17:00:58 -0500

    Fix  CI for cluster plotting

--- .ci-workflows/synthetic-dataset-plot-job.sh
+++ .ci-workflows/synthetic-dataset-plot-job.sh
@@ -3,7 +3,7 @@ ls
 echo "############### Moving to python folder"
 cd python/
 echo "############### Running test processor"
-python  jet_clustering/make_jet_splitting_PDFs.py analysis/hists/test_synthetic_datasets_all.coffea  --out jet_clustering/jet-splitting-PDFs-test
+python  jet_clustering/make_jet_splitting_PDFs.py analysis/hists/test_synthetic_datasets.coffea  --out jet_clustering/jet-splitting-PDFs-test
 echo "############### Checking if pdf files exist"
 ls jet_clustering/jet-splitting-PDFs-test/clustering_pdfs_vs_pT.yml 
 ls jet_clustering/jet-splitting-PDFs-test/test_sampling_pt_bb_mA.pdf 
