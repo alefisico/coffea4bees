bd108008f72c0c44df21cdeb252bcfb8f1d3c8d6
algomez <alejandro.gomez@cern.ch>
2024-08-27 09:40:35 -0500

    testing ci

--- .ci-workflows/skimmer-basic-test-job.sh
+++ .ci-workflows/skimmer-basic-test-job.sh
@@ -8,7 +8,7 @@ export BASE=skimmer/tests/
 echo "############### Changing metadata"
 sed -e "s#base_.*#base_path: \/builds\/${CI_PROJECT_PATH}\/python\/skimmer\/#" > ${BASE}modify_branches.yml
 cat ${BASE}modify_branches.yml
-sed -e "s#/GluGluToHHTo4B_cHHH1_TuneCP5_PSWeights_13TeV-powheg-pythia8/RunIISummer20UL18NanoAODv9.*#\n\t - root://cms-xrd-global.cern.ch//store/mc/RunIISummer20UL18NanoAODv9/GluGluToHHTo4B_cHHH1_TuneCP5_PSWeights_13TeV-powheg-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/2810000/94DA5440-3B94-354B-A25B-78518A52D2D1.root#" metadata/datasets_HH4b_yml > metadata/datasets_ci.yml
+sed -e "s#/GluGluToHHTo4B_cHHH1_TuneCP5_PSWeights_13TeV-powheg-pythia8/RunIISummer20UL18NanoAODv9.*#\n\t - root://cms-xrd-global.cern.ch//store/mc/RunIISummer20UL18NanoAODv9/GluGluToHHTo4B_cHHH1_TuneCP5_PSWeights_13TeV-powheg-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/2810000/94DA5440-3B94-354B-A25B-78518A52D2D1.root#" metadata/datasets_HH4b.yml > metadata/datasets_ci.yml
 cat metadata/datasets_ci.yml
 echo "############### Skimming"
 python runner.py -s -p ${BASE}modify_branches.py -c ${BASE}modify_branches_skimmer.yml -y UL18 -d GluGluToHHTo4B_cHHH1 -op ${BASE} -o picoAOD_modify_branches.yml -m metadata/datasets_ci.yml -t --debug
