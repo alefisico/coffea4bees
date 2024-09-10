94cd3a4ed7451ba34d442b123052db29d4a919b9
John Alison <johnalison@cmu.edu>
2024-09-10 13:49:46 -0500

    Debuugin CI v9

--- python/jet_clustering/sample_jet_templates.py
+++ python/jet_clustering/sample_jet_templates.py
@@ -99,17 +99,15 @@ def sample_PDFs_vs_pT(input_jets_decluster, input_pdfs, rand_seed, splittings, c
 
             # _num_samples
             counter = np.zeros((_num_samples, 3), dtype=np.uint64)  # split_name_hash_, pt, var_name
-            counter[:, 0] = np.asarray(pts ).view(np.uint64)
-            counter[:, 1] = np.asarray(etas).view(np.uint64)
-            counter[:, 2] = np.asarray(phis).view(np.uint64)
-
-
+            counter[:, 0] = np.round(np.asarray(pts ), 4).view(np.uint64)
+            counter[:, 1] = np.round(np.asarray(etas), 4).view(np.uint64)
+            counter[:, 2] = np.round(np.asarray(phis), 4).view(np.uint64)
 
             print(f"{chunk} sample_PDFs_vs_pT {_iVar} {rand_seed} {_splitting_name}\n")
             rng = Squares("sample_jet_templates", _iVar, rand_seed, _splitting_name)
 
-            print(f"{chunk} eta view: {np.asarray(etas).view(np.uint64)}\n")
             print(f"{chunk} eta: {np.asarray(etas)}\n")
+            print(f"{chunk} eta view: {np.round(np.asarray(etas),4).view(np.uint64)}\n")
 
             print(f"{chunk} counter {counter[0:5]}\n")
 
