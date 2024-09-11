262cb6c4d02202e058cf3555abc6acb71d354658
John Alison <johnalison@cmu.edu>
2024-09-11 10:15:00 -0500

    Changing skimmer to HH

--- .ci-workflows/skimmer-analysis-test-job.sh
+++ .ci-workflows/skimmer-analysis-test-job.sh
@@ -9,15 +9,15 @@ if [[ $(hostname) = *fnal* ]]; then
 else
     echo "############### Modifying previous dataset file (to read local files)"
     ls -lR skimmer/
-    sed -i "s/\/builds\/algomez\/coffea4bees\/python\///" skimmer/metadata/picoaod_datasets_TTToSemiLeptonic_UL18.yml
-    cat skimmer/metadata/picoaod_datasets_TTToSemiLeptonic_UL18.yml
+    sed -i "s|\/builds/$CI_PROJECT_PATH\/python\/||g"  skimmer/metadata/picoaod_datasets_GluGluToHHTo4B_cHHH0_UL18.yml
+    cat skimmer/metadata/picoaod_datasets_GluGluToHHTo4B_cHHH0_UL18.yml
 fi
 echo "############### Modifying dataset file with skimmer ci output"
-python metadata/merge_yaml_datasets.py -m metadata/datasets_HH4b.yml -f skimmer/metadata/picoaod_datasets_TTToSemiLeptonic_UL18.yml -o metadata/datasets_ci.yml
+python metadata/merge_yaml_datasets.py -m metadata/datasets_HH4b.yml -f skimmer/metadata/picoaod_datasets_GluGluToHHTo4B_cHHH0_UL18.yml -o metadata/datasets_ci.yml
 cat metadata/datasets_ci.yml
 echo "############### Changing metadata"
 sed -e "s/apply_FvT.*/apply_FvT: false/" -e "s/apply_trig.*/apply_trigWeight: false/" -e "s/run_SvB.*/run_SvB: false/" -e "s/top_reco.*/top_reconstruction_override: 'fast'/"  analysis/metadata/HH4b.yml > analysis/metadata/tmp.yml
 cat analysis/metadata/tmp.yml
 echo "############### Running test processor"
-python runner.py -o test_skimmer.coffea -d TTToSemiLeptonic -p analysis/processors/processor_HH4b.py -y UL18 -op analysis/hists/ -c analysis/metadata/tmp.yml -m metadata/datasets_ci.yml
+python runner.py -o test_skimmer.coffea -d GluGluToHHTo4B_cHHH0 -p analysis/processors/processor_HH4b.py -y UL18 -op analysis/hists/ -c analysis/metadata/tmp.yml -m metadata/datasets_ci.yml
 cd ../
