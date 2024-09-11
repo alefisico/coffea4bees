f8abc25a1d8b4a94f948f899f1583bfb5a3c2e3e
John Alison <johnalison@cmu.edu>
2024-09-11 14:58:41 -0500

    First draft of subsampling script

---
+++ .ci-workflows/sub-sample-dataset-make-dataset.sh
@@ -0,0 +1,17 @@
+echo "############### Including proxy"
+export X509_USER_PROXY=${PWD}/proxy/x509_proxy
+echo "############### Checking proxy"
+voms-proxy-info
+echo "############### Moving to python folder"
+cd python/
+echo "############### Changing metadata"
+if [[ $(hostname) = *fnal* ]]; then
+    sed -e "s#base_path.*#base_path: \/srv\/python\/skimmer\/test\/#" -e "s/\#max.*/maxchunks: 5/" -e "s/\#test.*/test_files: 1/" -e "s/\workers:.*/workers: 1/" -e "s/chunksize:.*/chunksize: 100000/"  skimmer/metadata/sub_sampling_MC.yml > skimmer/metadata/sub_sampling_MC_for_test.yml
+else
+    sed -e "s#base_.*#base_path: \/builds\/${CI_PROJECT_PATH}\/python\/skimmer\/#" -e "s/\#max.*/maxchunks: 1/" -e "s/\#test.*/test_files: 1/" -e "s/\workers:.*/workers: 1/" -e "s/chunksize:.*/chunksize: 1000/" -e "s/T3_US_FNALLPC/T3_CH_PSI/" skimmer/metadata/sub_sampling_MC.yml > skimmer/metadata/sub_sampling_MC_for_test.yml
+fi
+cat skimmer/metadata/sub_sampling_MC_for_test.yml
+echo "############### Running test processor"
+time python runner.py -s -p skimmer/processor/sub_sample_MC.py -c skimmer/metadata/sub_sampling_MC_for_test.yml -y UL18  -d TTToHadronic -op skimmer/metadata/ -o picoaod_datasets_TTToHadronic_pseudodata_test_UL18.yml -m metadata/datasets_HH4b.yml
+ls -R skimmer/
+cd ../
