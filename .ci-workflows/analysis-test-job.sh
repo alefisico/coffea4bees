7f72de93052d81ea9fdb4653bacd3f513e28e1c3
algomez <alejandro.gomez@cern.ch>
2024-10-14 16:16:39 -0500

    improving CI III

--- .ci-workflows/analysis-test-job.sh
+++ .ci-workflows/analysis-test-job.sh
@@ -1,22 +1,33 @@
 echo "############### Including proxy"
+if [ ! -f "${PWD}/proxy/x509_proxy" ]; then
+    echo "Error: x509_proxy file not found!"
+    exit 1
+fi
 export X509_USER_PROXY=${PWD}/proxy/x509_proxy
+
 echo "############### Checking proxy"
 voms-proxy-info
+
 echo "############### Moving to python folder"
 cd python/
+
+OUTPUT_DIR="output/analysis_test_job"
+echo "############### Checking and creating output directory"
+if [ ! -d $OUTPUT_DIR ]; then
+    mkdir -p $OUTPUT_DIR
+fi
+
+# echo "############### Modifying config"
 if [[ $(hostname) = *fnal* ]]; then
     DATASETS=metadata/datasets_HH4b.yml
 else
     DATASETS=metadata/datasets_HH4b_cernbox.yml
-    #echo "############### Modifying config"
-    #sed -e "s/condor_cores.*/condor_cores: 6/" -e "s/condor_memory.*/condor_memory: 8GB/" -i analysis/metadata/HH4b.yml
-    #cat analysis/metadata/HH4b.yml
 fi
-DATASETS=metadata/datasets_HH4b_cernbox.yml
+
 echo "############### Running datasets from " $DATASETS
 echo "############### Running test processor"
-python runner.py -t -o test.coffea -d data TTToHadronic TTToSemiLeptonic TTTo2L2Nu  ZH4b ZZ4b GluGluToHHTo4B_cHHH1 -p analysis/processors/processor_HH4b.py -y UL17 UL18 UL16_preVFP UL16_postVFP -op analysis/hists/ -m $DATASETS
+python runner.py -t -o test.coffea -d data TTToHadronic TTToSemiLeptonic TTTo2L2Nu  ZH4b ZZ4b GluGluToHHTo4B_cHHH1 -p analysis/processors/processor_HH4b.py -y UL17 UL18 UL16_preVFP UL16_postVFP -op $OUTPUT_DIR -m $DATASETS
 
-#python runner.py -t -o test.coffea -d data  -p analysis/processors/processor_HH4b.py -y UL18   -op analysis/hists/ -m $DATASETS
-ls
+#python runner.py -t -o test.coffea -d data  -p analysis/processors/processor_HH4b.py -y UL18   -op $OUTPUT_DIR -m $DATASETS
+ls $OUTPUT_DIR
 cd ../
