8149b1f620be414af05d64ad44737c957b613c58
algomez <alejandro.gomez@cern.ch>
2024-10-15 10:30:09 -0500

    improving CI III

--- .ci-workflows/skimmer-analysis-test-job.sh
+++ .ci-workflows/skimmer-analysis-test-job.sh
@@ -1,23 +1,38 @@
 echo "############### Including proxy"
+if [ ! -f "${PWD}/proxy/x509_proxy" ]; then
+    echo "Error: x509_proxy file not found!"
+    exit 1
+fi
 export X509_USER_PROXY=${PWD}/proxy/x509_proxy
+
 echo "############### Checking proxy"
 voms-proxy-info
+
 echo "############### Moving to python folder"
 cd python/
+
+INPUT_DIR="output/skimmer_test_job"
+OUTPUT_DIR="output/skimmer_analysis_test_job"
+echo "############### Checking and creating output directory"
+if [ ! -d $OUTPUT_DIR ]; then
+    mkdir -p $OUTPUT_DIR
+fi
+
 if [[ $(hostname) = *fnal* ]]; then
     echo "No changing files"
 else
     echo "############### Modifying previous dataset file (to read local files)"
-    ls -lR skimmer/
-    sed -i "s|\/builds/$CI_PROJECT_PATH\/python\/||g"  skimmer/metadata/picoaod_datasets_GluGluToHHTo4B_cHHH0_UL18.yml
-    cat skimmer/metadata/picoaod_datasets_GluGluToHHTo4B_cHHH0_UL18.yml
+    sed -i "s|\/builds/$CI_PROJECT_PATH\/python\/||g" $INPUT_DIR/picoaod_datasets_GluGluToHHTo4B_cHHH0_UL18.yml
+    cat $INPUT_DIR/picoaod_datasets_GluGluToHHTo4B_cHHH0_UL18.yml
 fi
 echo "############### Modifying dataset file with skimmer ci output"
-python metadata/merge_yaml_datasets.py -m metadata/datasets_HH4b.yml -f skimmer/metadata/picoaod_datasets_GluGluToHHTo4B_cHHH0_UL18.yml -o metadata/datasets_ci.yml
-cat metadata/datasets_ci.yml
+python metadata/merge_yaml_datasets.py -m $INPUT_DIR/datasets_HH4b.yml -f $INPUT_DIR/picoaod_datasets_GluGluToHHTo4B_cHHH0_UL18.yml -o $OUTPUT_DIR/datasets_HH4b.yml
+cat $OUTPUT_DIR/datasets_HH4b.yml
+
 echo "############### Changing metadata"
-sed -e "s/apply_FvT.*/apply_FvT: false/" -e "s/apply_trig.*/apply_trigWeight: false/" -e "s/run_SvB.*/run_SvB: false/" -e "s/top_reco.*/top_reconstruction_override: 'fast'/"  analysis/metadata/HH4b.yml > analysis/metadata/tmp.yml
-cat analysis/metadata/tmp.yml
+sed -e "s/apply_FvT.*/apply_FvT: false/" -e "s/apply_trig.*/apply_trigWeight: false/" -e "s/run_SvB.*/run_SvB: false/" -e "s/top_reco.*/top_reconstruction_override: 'fast'/"  analysis/metadata/HH4b.yml > $OUTPUT_DIR/HH4b.yml
+cat $OUTPUT_DIR/HH4b.yml
+
 echo "############### Running test processor"
-python runner.py -o test_skimmer.coffea -d GluGluToHHTo4B_cHHH0 -p analysis/processors/processor_HH4b.py -y UL18 -op analysis/hists/ -c analysis/metadata/tmp.yml -m metadata/datasets_ci.yml
+python runner.py -o test_skimmer.coffea -d GluGluToHHTo4B_cHHH0 -p analysis/processors/processor_HH4b.py -y UL18 -op $OUTPUT_DIR -c $OUTPUT_DIR/HH4b.yml -m $OUTPUT_DIR/datasets_HH4b.yml
 cd ../
--- .ci-workflows/synthetic-dataset-analyze.sh
+++ .ci-workflows/synthetic-dataset-analyze.sh
@@ -1,11 +1,26 @@
 echo "############### Including proxy"
+if [ ! -f "${PWD}/proxy/x509_proxy" ]; then
+    echo "Error: x509_proxy file not found!"
+    exit 1
+fi
 export X509_USER_PROXY=${PWD}/proxy/x509_proxy
+
 echo "############### Checking proxy"
 voms-proxy-info
+
 echo "############### Moving to python folder"
 cd python/
+
+INPUT_DIR="output/synthetic_dataset_make_dataset"
+OUTPUT_DIR="output/synthetic_dataset_analyze"
+echo "############### Checking and creating output directory"
+if [ ! -d $OUTPUT_DIR ]; then
+    mkdir -p $OUTPUT_DIR
+fi
+
+echo "############### Modifying config"
 if [[ $(hostname) = *fnal* ]]; then
-    echo "No changing files"
+    sed -e "s|skimmer/test|$INPUT_DIR|" metadata/datasets_synthetic_test.yml > $OUTPUT_DIR/datasets_synthetic_test.yml
 else
     echo "############### Modifying previous dataset file (to read local files)"
     ls -lR skimmer/
@@ -13,9 +28,9 @@ else
     cat metadata/datasets_synthetic_test.yml
     pwd
     echo ${CI_PROJECT_PATH}
-    sed -i  -e "s|\/srv/python\/||g" -e "s|test\/||g"  metadata/datasets_synthetic_test.yml
+    sed -e "s|\/srv/python\/||g" -e "s|test\/||g"  metadata/datasets_synthetic_test.yml > $OUTPUT_DIR/datasets_synthetic_test.yml
     echo "NEW"
-    cat metadata/datasets_synthetic_test.yml
+    cat $OUTPUT_DIR/datasets_synthetic_test.yml
 fi
 # echo "############### Modifying dataset file with skimmer ci output"
 # cat metadata/datasets_ci.yml
@@ -28,10 +43,10 @@ fi
 
 
 # echo "############### Changing metadata"
-# sed -e "s/apply_FvT.*/apply_FvT: false/" -e "s/apply_trig.*/apply_trigWeight: false/" -e "s/run_SvB.*/run_SvB: false/"  analysis/metadata/HH4b.yml > analysis/metadata/tmp.yml
-# cat analysis/metadata/tmp.yml
+# sed -e "s/apply_FvT.*/apply_FvT: false/" -e "s/apply_trig.*/apply_trigWeight: false/" -e "s/run_SvB.*/run_SvB: false/"  analysis/metadata/HH4b.yml > $OUTPUT_DIR/tmp.yml
+# cat $OUTPUT_DIR/tmp.yml
 #echo "############### Running test processor"
-#time python runner.py -o test_synthetic_data_test.coffea -d data -p analysis/processors/processor_HH4b.py -y UL18  -op analysis/hists/ -c analysis/metadata/HH4b_synthetic_data.yml -m metadata/datasets_synthetic_seed17.yml
+#time python runner.py -o test_synthetic_data_test.coffea -d data -p analysis/processors/processor_HH4b.py -y UL18  -op $OUTPUT_DIR/ -c $OUTPUT_DIR/HH4b_synthetic_data.yml -m metadata/datasets_synthetic_seed17.yml
 
 
 echo "############### Running test processor "
@@ -39,7 +54,7 @@ echo "############### Running test processor "
 # python metadata/merge_yaml_datasets.py -m metadata/datasets_synthetic_seed17.yml -f skimmer/metadata/picoaod_datasets_declustered_GluGluToHHTo4B_cHHH1_Run2_seed17.yml -o metadata/datasets_synthetic_seed17.yml
 #cat metadata/datasets_synthetic_test.yml
 
-time python runner.py -o test_synthetic_datasets.coffea -d synthetic_data synthetic_mc_GluGluToHHTo4B_cHHH1 -p analysis/processors/processor_HH4b.py -y UL18  -op analysis/hists/ -c analysis/metadata/HH4b_synthetic_data.yml -m metadata/datasets_synthetic_test.yml
+time python runner.py -o test_synthetic_datasets.coffea -d synthetic_data synthetic_mc_GluGluToHHTo4B_cHHH1 -p analysis/processors/processor_HH4b.py -y UL18  -op $OUTPUT_DIR/ -c analysis/metadata/HH4b_synthetic_data.yml -m $OUTPUT_DIR/datasets_synthetic_test.yml
 
-# time python runner.py -o test_synthetic_datasets.coffea -d data GluGluToHHTo4B_cHHH1 -p analysis/processors/processor_HH4b.py -y UL18  -op analysis/hists/ -c analysis/metadata/HH4b_synthetic_data.yml -m metadata/datasets_synthetic_test.yml
+# time python runner.py -o test_synthetic_datasets.coffea -d data GluGluToHHTo4B_cHHH1 -p analysis/processors/processor_HH4b.py -y UL18  -op $OUTPUT_DIR/ -c analysis/metadata/HH4b_synthetic_data.yml -m $OUTPUT_DIR/datasets_synthetic_test.yml
 cd ../
