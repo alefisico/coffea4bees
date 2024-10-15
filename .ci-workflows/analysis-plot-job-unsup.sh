6ddf96e74bf8dd05c32c36e9b0a22d7d3f1e37b8
algomez <alejandro.gomez@cern.ch>
2024-10-15 14:55:22 -0500

    improving CI V

--- .ci-workflows/analysis-plot-job-unsup.sh
+++ .ci-workflows/analysis-plot-job-unsup.sh
@@ -1,12 +1,18 @@
-echo "############### Checking ls"
-ls
 echo "############### Moving to python folder"
 cd python/
+
+INPUT_DIR="output/analysis_test_job_unsup"
+OUTPUT_DIR="output/analysis_plot_job_unsup"
+echo "############### Checking and creating output directory"
+if [ ! -d $OUTPUT_DIR ]; then
+    mkdir -p $OUTPUT_DIR
+fi
+
 echo "############### Running test processor"
-python analysis/makePlots_unsup.py analysis/hists/test_unsup.coffea --doTest   -o analysis/testCoffeaPlots_unsup/ -m analysis/metadata/plotsAll_unsup.yml 
+python analysis/makePlots_unsup.py $INPUT_DIR/test_unsup.coffea --doTest   -o $OUTPUT_DIR/ -m analysis/metadata/plotsAll_unsup.yml 
 echo "############### Checking if pdf files exist"
-ls analysis/testCoffeaPlots_unsup/RunII/passPreSel/fourTag/SR/mix_v0/v4j_mass.pdf
-ls analysis/testCoffeaPlots_unsup/RunII/passPreSel/fourTag/SR_vs_SB/mix_v0/v4j_mass.pdf
-ls analysis/testCoffeaPlots_unsup/RunII/passPreSel/fourTag/SR/mix_v0/quadJet_selected_lead_vs_subl_m.pdf 
-ls analysis/testCoffeaPlots_unsup/RunII/passPreSel/threeTag/SR/data_3b_for_mixed/quadJet_selected_lead_vs_subl_m.pdf 
+ls $OUTPUT_DIR/RunII/passPreSel/fourTag/SR/mix_v0/v4j_mass.pdf
+ls $OUTPUT_DIR/RunII/passPreSel/fourTag/SR_vs_SB/mix_v0/v4j_mass.pdf
+ls $OUTPUT_DIR/RunII/passPreSel/fourTag/SR/mix_v0/quadJet_selected_lead_vs_subl_m.pdf 
+ls $OUTPUT_DIR/RunII/passPreSel/threeTag/SR/data_3b_for_mixed/quadJet_selected_lead_vs_subl_m.pdf 
 cd ../
