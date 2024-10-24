88d2ea3bff32267fa38c6e5c237fa661d89edecc
ChuyuanLiu <chuyuanliu96@gmail.com>
2024-10-24 09:21:37 -0400

    classifier: minor improvements

--- python/classifier/task/parse/_dict.py
+++ python/classifier/task/parse/_dict.py
@@ -3,12 +3,14 @@ from collections import defaultdict
 from functools import cache
 from io import StringIO
 from pathlib import Path
-from typing import overload
+from typing import Literal, overload
 
 import fsspec
 
-_SCHEME = ":##"
+_SCHEMA = ":##"
 _KEY = "@@"
+
+SupportedSchema = Literal["yaml", "json", "csv", "file", "py"]
 
 
 class DeserializationError(Exception):
@@ -18,8 +20,8 @@ class DeserializationError(Exception):
         self.msg = msg
 
 
-def _mapping_scheme(arg: str):
-    arg = arg.split(_SCHEME, 1)
+def _mapping_schema(arg: str):
+    arg = arg.split(_SCHEMA, 1)
     if len(arg) == 1:
         return None, arg[0]
     else:
@@ -85,15 +87,31 @@ def _deserialize_file(path: str, formatter: str):
         raise DeserializationError(f'Failed to read file "{path}"')
 
 
+def _fetch_key(mapping, key):
+    try:
+        return mapping[key]
+    except Exception:
+        ...
+    try:
+        return mapping[int(key)]
+    except Exception:
+        ...
+    raise KeyError()
+
+
 def escape(obj) -> str:
     if not isinstance(obj, str):
         import json
 
-        obj = f"json{_SCHEME}{json.dumps(obj)}"
+        obj = f"json{_SCHEMA}{json.dumps(obj)}"
     return obj
 
 
-def mapping(arg: str, default: str = "yaml", formatter: str = None):
+def mapping(
+    arg: str,
+    default: SupportedSchema = "yaml",
+    formatter: str = None,
+):
     """
     - `{data}`: parse as yaml
     - `yaml:##{data}`: parse as yaml
@@ -112,7 +130,7 @@ def mapping(arg: str, default: str = "yaml", formatter: str = None):
     def warn(msg: str):
         logging.warning(f'{msg} when parsing "{arg}"')
 
-    protocol, data = _mapping_scheme(arg)
+    protocol, data = _mapping_schema(arg)
     if protocol is None:
         protocol = default
     keys = None
@@ -129,8 +147,8 @@ def mapping(arg: str, default: str = "yaml", formatter: str = None):
     if keys is not None:
         for i, k in enumerate(keys):
             try:
-                result = result[k]
-            except Exception:
+                result = _fetch_key(result, k)
+            except KeyError:
                 warn(f'Failed to select key "{".".join(keys[:i+1])}"')
                 return
     return result
