7f3563ec0c4c3eaad76cf660041e5b661a09e179
ChuyuanLiu <chuyuanliu96@gmail.com>
2024-10-30 23:23:50 -0400

    classifier: improve utils

--- python/classifier/task/parse/_class.py
+++ python/classifier/task/parse/_class.py
@@ -3,13 +3,13 @@ from ...utils import import_
 from ._dict import mapping
 
 
-def instance(__opt: list[str], __pkg: str, **__kwargs):
-    if len(__opt) < 1:
+def instance(opt: list[str], pkg: str, /, **kwargs):
+    if len(opt) < 1:
         return None
-    parts = f"{__pkg}.{__opt[0]}".split(".")
+    parts = f"{pkg}.{opt[0]}".split(".")
     _, cls = import_(".".join(parts[:-1]), parts[-1])
     if cls is None:
         return None
-    if len(__opt) > 1:
-        dict_proxy(__kwargs).update(*map(mapping, __opt[1:]))
-    return cls(**__kwargs)
+    if len(opt) > 1:
+        dict_proxy(kwargs).update(*map(mapping, opt[1:]))
+    return cls(**kwargs)
