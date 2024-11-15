30bcf9fc587740f6b9c2cbd44d6bc4bd98bef170
ChuyuanLiu <chuyuanliu96@gmail.com>
2024-11-14 23:55:09 -0500

    classifier: fix FvT baseline script

--- python/classifier/config/workflows/HCR/FvT_baseline/run_mixed.sh
+++ python/classifier/config/workflows/HCR/FvT_baseline/run_mixed.sh
@@ -9,7 +9,7 @@ export WEB="root://eosuser.cern.ch//eos/user/${CERNUSER}/www/HH4b/classifier/FvT
 # train mixed and make plots
 for i in {0..14}
 do
-    ./pyml.py template "{mixed: ${i}, offset: 0, user: ${LPCUSER}, tag: mixed}" $WFS/train.yml
+    ./pyml.py template "{mixed: ${i}, offset: 0, user: ${LPCUSER}, tag: mixed}" $WFS/train_mixed.yml
     ./pyml.py analyze --results ${MODEL}/mixed-${i}/result.json -analysis HCR.LossROC -setting IO "output: ${WEB}" -setting IO "report: mixed-${i}"
 done
 # evaluate
