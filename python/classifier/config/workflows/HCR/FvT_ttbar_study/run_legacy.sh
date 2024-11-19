edbfe2392e03c16c29c82a54a85c74fe06bbf4de
chuyuanl <chuyuanl@andrew.cmu.edu>
2024-11-18 21:53:05 -0500

    classifier: add FvT ttbar study for legacy dataset

---
+++ python/classifier/config/workflows/HCR/FvT_ttbar_study/run_legacy.sh
@@ -0,0 +1,18 @@
+export LPCUSER="chuyuanl"
+export CERNUSER="c/chuyuan"
+export WFS="classifier/config/workflows/HCR/FvT_ttbar_study"
+export BASE="root://cmseos.fnal.gov//store/user/${LPCUSER}/HH4b"
+export MODEL="${BASE}/classifier/FvT/nott/legacy/"
+export WEB="root://eosuser.cern.ch//eos/user/${CERNUSER}/www/HH4b/classifier/FvT_ttbar_study/legacy/"
+
+# train mixed and make plots
+for i in {0..14}
+do
+    ./pyml.py template "{mixed: ${i}, offset: 0, user: ${LPCUSER}, tag: mixed}" $WFS/train_legacy.yml
+    ./pyml.py analyze --results ${MODEL}/mixed-${i}/result.json -analysis HCR.LossROC -setting IO "output: ${WEB}" -setting IO "report: nott-${i}"
+done
+# evaluate
+for i in {0..14}
+do
+    ./pyml.py template "{mixed: ${i}, tag: mixed, user: ${LPCUSER}}" $WFS/evaluate_legacy.yml
+done
