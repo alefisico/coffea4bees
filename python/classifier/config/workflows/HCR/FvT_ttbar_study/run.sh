279f71097158aa8ae9e3f03d2dd16dd4ca601181
chuyuanl <chuyuanl@andrew.cmu.edu>
2024-11-19 00:31:13 -0500

    classifier: update FvT ttbar study scripts

--- python/classifier/config/workflows/HCR/FvT_ttbar_study/run.sh
+++ python/classifier/config/workflows/HCR/FvT_ttbar_study/run.sh
@@ -5,14 +5,22 @@ export BASE="root://cmseos.fnal.gov//store/user/${LPCUSER}/HH4b"
 export MODEL="${BASE}/classifier/FvT/nott"
 export WEB="root://eosuser.cern.ch//eos/user/${CERNUSER}/www/HH4b/classifier/FvT_ttbar_study/"
 
+# check first argument
+if [ -z "$1" ]; then
+    port=10200
+else
+    port=$1
+fi
+
+
 # train mixed and make plots
 for i in {0..14}
 do
-    ./pyml.py template "{mixed: ${i}, offset: 0, user: ${LPCUSER}, tag: mixed}" $WFS/train.yml
-    ./pyml.py analyze --results ${MODEL}/mixed-${i}/result.json -analysis HCR.LossROC -setting IO "output: ${WEB}" -setting IO "report: nott-${i}"
+    ./pyml.py template "{mixed: ${i}, offset: 0, user: ${LPCUSER}, tag: mixed}" $WFS/train.yml -setting Monitor "address: :${port}"
+    ./pyml.py analyze --results ${MODEL}/mixed-${i}/result.json -analysis HCR.LossROC -setting IO "output: ${WEB}" -setting IO "report: mixed-${i}" -setting Monitor "address: :${port}"
 done
 # evaluate
 for i in {0..14}
 do
-    ./pyml.py template "{mixed: ${i}, tag: mixed, user: ${LPCUSER}}" $WFS/evaluate.yml
+    ./pyml.py template "{mixed: ${i}, tag: mixed, user: ${LPCUSER}}" $WFS/evaluate.yml -setting Monitor "address: :${port}"
 done
