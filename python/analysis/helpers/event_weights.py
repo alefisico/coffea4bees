23e716b09b7f9d52b5e310182f5189d904d74a51
John Alison <johnalison@cmu.edu>
2024-11-22 08:33:17 -0600

    Adding a printout

--- python/analysis/helpers/event_weights.py
+++ python/analysis/helpers/event_weights.py
@@ -183,6 +183,7 @@ def add_pseudotagweights( selev, weights,
                         selev.weight,
                     )
                 weight = np.full(len_event, 1.0)
+                print(f"Applying threetag weight JCM: {_JCM_load} FvT: {event_metadata['FvT_names'][0]}\n")
                 weight[analysis_selections] = np.where(selev.threeTag, getattr(selev, f"{_JCM_load}") * getattr(getattr(selev, event_metadata["FvT_names"][0]), event_metadata["FvT_names"][0]), 1.0)
                 weights.add("FvT", weight)
                 list_weight_names.append("FvT")
