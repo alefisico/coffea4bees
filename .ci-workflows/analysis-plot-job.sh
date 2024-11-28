d494a70d683ec07db76804286efa5ab43adb5f73
John Alison <johnda102@gmail.com>
2024-11-28 11:33:26 -0500

    Factorizing the plotting code a bit

--- .ci-workflows/analysis-plot-job.sh
+++ .ci-workflows/analysis-plot-job.sh
@@ -25,7 +25,7 @@ ls $OUTPUT_DIR/RunII/passPreSel/threeTag/SR/Multijet/quadJet_min_dr_close_vs_oth
 
 
 echo "############### check making the plots from yaml "
-py analysis/plot_from_yaml.py --input_yaml $OUTPUT_DIR/RunII/passPreSel/fourTag/SR/SvB_MA_ps_zz.yaml \
+python analysis/plot_from_yaml.py --input_yaml $OUTPUT_DIR/RunII/passPreSel/fourTag/SR/SvB_MA_ps_zz.yaml \
    $OUTPUT_DIR/RunII/passPreSel/fourTag/SR/SvB_MA_ps_zh.yaml \
    $OUTPUT_DIR/RunII/passPreSel/fourTag/SR/SvB_MA_ps_hh.yaml \
    $OUTPUT_DIR/RunII/passPreSel/fourTag/SR_vs_SB/data/SvB_MA_ps.yaml \
