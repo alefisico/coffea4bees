4068063a0e696f8be588edbe1ca4de8cd6b2cd56
ChuyuanLiu <chuyuanliu96@gmail.com>
2024-12-02 15:23:32 -0500

    classifier: add more workflows

--- python/classifier/config/workflows/HCR/FvT_study/run.sh
+++ python/classifier/config/workflows/HCR/FvT_study/run.sh
@@ -16,11 +16,21 @@ fi
 # train mixed and make plots
 for i in {0..14}
 do
-    ./pyml.py template "{mixed: ${i}, offset: 0, user: ${LPCUSER}, tag: mixed, name: $1}" $WFS/train_$1.yml -setting Monitor "address: :${port}"
-    ./pyml.py analyze --results ${MODEL}/mixed-${i}/result.json -analysis HCR.LossROC -setting IO "output: ${WEB}" -setting IO "report: mixed-${i}" -setting Monitor "address: :${port}"
+    ./pyml.py \
+    template "{mixed: ${i}, user: ${LPCUSER}, name: $1}" $WFS/train.yml \
+    -template "{mixed: ${i}, user: ${LPCUSER}, offset: 0} $WFS/train_$1.yml" \
+    -setting Monitor "address: :${port}"
+
+    ./pyml.py analyze --results ${MODEL}/mixed-${i}/result.json \
+    -analysis HCR.LossROC \
+    -setting IO "output: ${WEB}" \
+    -setting IO "report: mixed-${i}" \
+    -setting Monitor "address: :${port}"
 done
 # evaluate
 for i in {0..14}
 do
-    ./pyml.py template "{mixed: ${i}, tag: mixed, user: ${LPCUSER}, name: $1}" $WFS/evaluate_$1.yml -setting Monitor "address: :${port}"
+    ./pyml.py \
+    template "{mixed: ${i}, user: ${LPCUSER}, name: $1}" $WFS/evaluate.yml \
+    -setting Monitor "address: :${port}"
 done
