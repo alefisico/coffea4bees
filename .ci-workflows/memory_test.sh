d168ff89d6a83f543145722b656277f78a828d75
algomez <alejandro.gomez@cern.ch>
2024-12-05 14:20:35 -0600

    fix

--- .ci-workflows/memory_test.sh
+++ .ci-workflows/memory_test.sh
@@ -8,7 +8,7 @@ if [ ! -d $OUTPUT_DIR ]; then
     mkdir -p $OUTPUT_DIR
 fi
 echo "############### Running memory test"
-python base_class/tests/memory_test.py --threshold 3689.422 -o $OUTPUT_DIR/mprofile_ci_test --script runner.py -o test.coffea -d GluGluToHHTo4B_cHHH1 -p analysis/processors/processor_HH4b.py -y UL18 -op local_outputs/analysis/ -m analysis/metadata/datasets_HH4b_cernbox.yml
+python base_class/tests/memory_test.py --threshold 3689.422 -o $OUTPUT_DIR/mprofile_ci_test --script runner.py -o test.coffea -d GluGluToHHTo4B_cHHH1 -p analysis/processors/processor_HH4b.py -y UL18 -op local_outputs/analysis/ -m metadata/datasets_HH4b_cernbox.yml
 ls $OUTPUT_DIR/mprofile_ci_test.png
 cd ../
 
