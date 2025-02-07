31cc72f37784967cb970cb140126d4ee00bed8fd
ChuyuanLiu <chuyuanliu96@gmail.com>
2025-02-07 16:48:59 -0500

    update memory test threshold

--- .ci-workflows/memory_test.sh
+++ .ci-workflows/memory_test.sh
@@ -9,7 +9,7 @@ fi
 
 echo "############### Running memory test"
 sed -e "s#  workers: 4.*#  workers: 1\n  maxchunks: 1#" analysis/metadata/HH4b_signals.yml > $OUTPUT_DIR/HH4b_memory_test.yml
-python base_class/tests/memory_test.py --threshold 3650 -o $OUTPUT_DIR/mprofile_ci_test --script runner.py -o test.coffea -d GluGluToHHTo4B_cHHH1 -p analysis/processors/processor_HH4b.py -y UL18 -op local_outputs/analysis/ -m $DATASETS -c $OUTPUT_DIR/HH4b_memory_test.yml
+python base_class/tests/memory_test.py --threshold 3500 --tolerance 20 -o $OUTPUT_DIR/mprofile_ci_test --script runner.py -o test.coffea -d GluGluToHHTo4B_cHHH1 -p analysis/processors/processor_HH4b.py -y UL18 -op local_outputs/analysis/ -m $DATASETS -c $OUTPUT_DIR/HH4b_memory_test.yml
 ls $OUTPUT_DIR/mprofile_ci_test.png
 
 if [ "$return_to_base" = true ]; then
