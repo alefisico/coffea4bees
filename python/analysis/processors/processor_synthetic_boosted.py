e98480dd735ef461e9fb728592063c8dd18e3a05
John Alison <johnda102@gmail.com>
2025-05-28 16:17:22 -0400

    Better fat jet histograms in test job

--- python/analysis/processors/processor_synthetic_boosted.py
+++ python/analysis/processors/processor_synthetic_boosted.py
@@ -10,6 +10,9 @@ from coffea.analysis_tools import PackedSelection
 import hist
 
 from analysis.helpers.event_selection import apply_event_selection
+from base_class.hist import Collection, Fill
+from jet_clustering.clustering_hist_templates import ClusterHists
+from base_class.physics.object import Jet
 
 import logging
 
@@ -56,40 +59,112 @@ class analysis(processor.ProcessorABC):
         #
         # Event selection
         #
-        jets = event.FatJet[event.FatJet.pt > 200]
+        event["selFatJet"] = event.FatJet[event.FatJet.pt > 300]
+
+
+
+##    # Create the PtEtaPhiMLorentzVectorArray
+##    splittings_events = ak.zip(
+##        {
+##            "pt":   ak.Array([[v.pt   for v in sublist] for sublist in splittings]),
+##            "eta":  ak.Array([[v.eta  for v in sublist] for sublist in splittings]),
+##            "phi":  ak.Array([[v.phi  for v in sublist] for sublist in splittings]),
+##            "mass": ak.Array([[v.mass for v in sublist] for sublist in splittings]),
+##            "jet_flavor": ak.Array([[v.jet_flavor for v in sublist] for sublist in splittings]),
+##            "btag_string": ak.Array([[v.btag_string for v in sublist] for sublist in splittings]),
+##            "part_A": ak.zip(
+##                {
+##                    "pt":         ak.Array([[v.part_A.pt   for v in sublist] for sublist in splittings]),
+##                    "eta":        ak.Array([[v.part_A.eta  for v in sublist] for sublist in splittings]),
+##                    "phi":        ak.Array([[v.part_A.phi  for v in sublist] for sublist in splittings]),
+##                    "mass":       ak.Array([[v.part_A.mass for v in sublist] for sublist in splittings]),
+##                    "jet_flavor": ak.Array([[v.part_A.jet_flavor for v in sublist] for sublist in splittings]),
+##                    "btag_string": ak.Array([[v.part_A.btag_string for v in sublist] for sublist in splittings]),
+##                },
+##                with_name="PtEtaPhiMLorentzVector",
+##                behavior=vector.behavior
+##            ),
+##            "part_B": ak.zip(
+##                {
+##                    "pt":         ak.Array([[v.part_B.pt  for v in sublist] for sublist in splittings]),
+##                    "eta":        ak.Array([[v.part_B.eta for v in sublist] for sublist in splittings]),
+##                    "phi":        ak.Array([[v.part_B.phi for v in sublist] for sublist in splittings]),
+##                    "mass":       ak.Array([[v.part_B.mass for v in sublist] for sublist in splittings]),
+##                    "jet_flavor": ak.Array([[v.part_B.jet_flavor for v in sublist] for sublist in splittings]),
+##                    "btag_string": ak.Array([[v.part_B.btag_string for v in sublist] for sublist in splittings]),
+##                },
+##                with_name="PtEtaPhiMLorentzVector",
+##                behavior=vector.behavior
+##            ),
+##        },
+##        with_name="PtEtaPhiMLorentzVector",
+##        behavior=vector.behavior
+##    )
+
+
+
 
         # Apply object selection (function does not remove events, adds content to objects)
-        
+
         selections = PackedSelection()
         selections.add( "lumimask", event.lumimask)
         selections.add( "passNoiseFilter", event.passNoiseFilter)
         selections.add( "passHLT", ( np.full(len(event), True) if isMC else event.passHLT ) )
-        selections.add( "num_jets", ( ak.num(jets) > 0 ) )
+        selections.add( "passNFatJets", ( ak.num(event.selFatJet) > 0 ) )
         ### add more selections, this can be useful
 
-        list_of_cuts = [ "lumimask", "passNoiseFilter", "passHLT", "num_jets" ]
-        
-        #
-        # Histograms
-        #
-        process_axis = hist.axis.StrCategory([], name="process", label="Process", growth=True)
-        sel_axis = hist.axis.StrCategory([], name="selection", label="Selection", growth=True)
-        year_axis = hist.axis.StrCategory([], name="year", label="Year", growth=True)
-        jet_pt = hist.axis.Regular(100, 0., 1000., name="jet_pt", label="Jet Pt [GeV]")
+        list_of_cuts = [ "lumimask", "passNoiseFilter", "passHLT", "passNFatJets" ]
 
-        output = {
-            'hists' : {
-                "jet_pt": hist.Hist(process_axis, sel_axis, year_axis, jet_pt),
-            }
-        }
 
-        output["hists"]["jet_pt"].fill(
-            process=processName,
-            selection="all",
-            year=year,
-            jet_pt=ak.flatten(jets.pt[selections.all(*list_of_cuts)]),
-        )
-        
+        #
+        # Better Hists
+        #
+
+        # Hacking the tag variable
+        event["fourTag"] = True
+        event['tag'] = ak.zip({
+            "fourTag": event.fourTag,
+        })
+
+
+        # Hack the region varable
+        event["SR"] = True
+        event["region"] = ak.zip({
+            "SR": event.SR,
+        })
+
+        event["weight"] = 1.0
+
+        event["passNFatJets"]  = (ak.num(event.selFatJet) > 0)
+
+        fill = Fill(process=processName, year=year, weight="weight")
+        histCuts = ["passNFatJets"]
+
+        hist = Collection( process=[processName],
+                           year=[year],
+                           tag=["fourTag"],  # 3 / 4/ Other
+                           region=['SR'],  # SR / SB / Other
+                           **dict((s, ...) for s in histCuts)
+                           )
+
+
+        #
+        # Jets
+        #
+        fill += Jet.plot(("fatJets", "Selected Fat Jets"),        "selFatJet",           skip=["deepjet_c"], bins={"pt": (50, 0, 1000)})
+
+#        for _s_type in cleaned_splitting_name:
+#            fill += ClusterHists( (f"splitting_{_s_type}", f"{_s_type} Splitting"), f"splitting_{_s_type}" )
+
+        #
+        # fill histograms
+        #
+        fill(event, hist)
+
+        processOutput = {}
+
+        output = hist.output | processOutput
+
         return output
 
     def postprocess(self, accumulator):
