f277d9dd3b0e0e3de79762d2f8b74e21c27b0e2f
John Alison <johnda102@gmail.com>
2025-05-29 16:00:42 -0400

    Update with local signal files and 1st pass (not working) of creating boosted splitting from subjets

--- python/analysis/processors/processor_synthetic_boosted.py
+++ python/analysis/processors/processor_synthetic_boosted.py
@@ -45,8 +45,8 @@ class analysis(processor.ProcessorABC):
         isMC    = True if event.run[0] == 1 else False
         nEvent = len(event)
 
-        logging.debug(fname)
-        logging.debug(f'Process {nEvent} Events')
+        logging.info(fname)
+        logging.info(f'Process {nEvent} Events')
 
         #
         # Event selection
@@ -56,50 +56,25 @@ class analysis(processor.ProcessorABC):
                                         cut_on_lumimask=True
                                         )
 
-        #
-        # Event selection
-        #
-        event["selFatJet"] = event.FatJet[event.FatJet.pt > 300]
+
+
+        selFatJet = event.FatJet[event.FatJet.pt > 300]
+        selFatJet = selFatJet[ak.num(selFatJet.subjets, axis=2) > 1]
+
+        print(f" fields FatJets: {selFatJet.fields}")
+        print(f" fields nSubJets: {selFatJet.subjets.fields}")
+        print(f" nSubJets: {ak.num(selFatJet.subjets, axis=1)}")
+
+        #selFatJet = selFatJet[ak.num(selFatJet.subjets) > 1]
+        event["selFatJet"] = selFatJet
+
+
+        #  Cehck How often do we have >=2 Fat Jets?
+        event["passNFatJets"]  = (ak.num(event.selFatJet) == 2)
 
 
 
-##    # Create the PtEtaPhiMLorentzVectorArray
-##    splittings_events = ak.zip(
-##        {
-##            "pt":   ak.Array([[v.pt   for v in sublist] for sublist in splittings]),
-##            "eta":  ak.Array([[v.eta  for v in sublist] for sublist in splittings]),
-##            "phi":  ak.Array([[v.phi  for v in sublist] for sublist in splittings]),
-##            "mass": ak.Array([[v.mass for v in sublist] for sublist in splittings]),
-##            "jet_flavor": ak.Array([[v.jet_flavor for v in sublist] for sublist in splittings]),
-##            "btag_string": ak.Array([[v.btag_string for v in sublist] for sublist in splittings]),
-##            "part_A": ak.zip(
-##                {
-##                    "pt":         ak.Array([[v.part_A.pt   for v in sublist] for sublist in splittings]),
-##                    "eta":        ak.Array([[v.part_A.eta  for v in sublist] for sublist in splittings]),
-##                    "phi":        ak.Array([[v.part_A.phi  for v in sublist] for sublist in splittings]),
-##                    "mass":       ak.Array([[v.part_A.mass for v in sublist] for sublist in splittings]),
-##                    "jet_flavor": ak.Array([[v.part_A.jet_flavor for v in sublist] for sublist in splittings]),
-##                    "btag_string": ak.Array([[v.part_A.btag_string for v in sublist] for sublist in splittings]),
-##                },
-##                with_name="PtEtaPhiMLorentzVector",
-##                behavior=vector.behavior
-##            ),
-##            "part_B": ak.zip(
-##                {
-##                    "pt":         ak.Array([[v.part_B.pt  for v in sublist] for sublist in splittings]),
-##                    "eta":        ak.Array([[v.part_B.eta for v in sublist] for sublist in splittings]),
-##                    "phi":        ak.Array([[v.part_B.phi for v in sublist] for sublist in splittings]),
-##                    "mass":       ak.Array([[v.part_B.mass for v in sublist] for sublist in splittings]),
-##                    "jet_flavor": ak.Array([[v.part_B.jet_flavor for v in sublist] for sublist in splittings]),
-##                    "btag_string": ak.Array([[v.part_B.btag_string for v in sublist] for sublist in splittings]),
-##                },
-##                with_name="PtEtaPhiMLorentzVector",
-##                behavior=vector.behavior
-##            ),
-##        },
-##        with_name="PtEtaPhiMLorentzVector",
-##        behavior=vector.behavior
-##    )
+
 
 
 
@@ -110,10 +85,78 @@ class analysis(processor.ProcessorABC):
         selections.add( "lumimask", event.lumimask)
         selections.add( "passNoiseFilter", event.passNoiseFilter)
         selections.add( "passHLT", ( np.full(len(event), True) if isMC else event.passHLT ) )
-        selections.add( "passNFatJets", ( ak.num(event.selFatJet) > 0 ) )
+        selections.add( "passNFatJets",  event.passNFatJets )
         ### add more selections, this can be useful
 
-        list_of_cuts = [ "lumimask", "passNoiseFilter", "passHLT", "passNFatJets" ]
+
+        #list_of_cuts = [ "lumimask", "passNoiseFilter", "passHLT", "passNFatJets" ]
+        list_of_cuts = [ "passNFatJets" ]
+        analysis_selections = selections.all(*list_of_cuts)
+        selev = event[analysis_selections]
+
+        #
+        # Event selection
+        #
+        #event["selFatJet"] = event.FatJet[event.FatJet. > 1]
+
+        print(f"Number of selected Fat Jets: {ak.num(selev.selFatJet)}")
+        print(f" Any passNFatJets: {ak.any(selev.passNFatJets)}")
+        print(f" Any passHLT: {ak.any(selev.passHLT)}")
+        print(f" FatJet pt: {selev.selFatJet.pt}")
+
+        print(f" nSubJets: {ak.num(selev.selFatJet.subjets, axis=2)}")
+        print(f" subjet pt: {selev.selFatJet.pt[0:10]}")
+
+        print(f" FatJet subjet pt: {selev.selFatJet.subjets.pt[0]}")
+        print(f" FatJet subjet0 pt: {selev.selFatJet[:,0].subjets.pt}")
+        print(f" FatJet subjet0_0 pt: {selev.selFatJet[:,0].subjets[:,0].pt}")
+        print(f" FatJet subjet0_1 pt: {selev.selFatJet[:,0].subjets[:,1].pt}")
+        print(f" FatJet subjet1 pt: {selev.selFatJet[:,1].subjets.pt}")
+        #print("SubJet 0",ak.Array([[v.pt   for v in sublist] for sublist in selev.selFatJet.subjets[:,0]]))
+        #print("SubJet 1",ak.Array([[v.pt   for v in sublist] for sublist in selev.selFatJet.subjets[:,1]]))
+
+        print( "v" , ak.Array([[v   for v in sublist] for sublist in selev.selFatJet[:,0].subjets[:,0]]) )
+
+        print( ak.Array([[v.pt   for v in sublist] for sublist in selev.selFatJet[:,0].subjets[:,0]]) )
+
+#        # Create the PtEtaPhiMLorentzVectorArray
+#        fat_jet_splittings_events = ak.zip(
+#            {
+#                "pt":   ak.Array([[v.pt   for v in sublist] for sublist in selev.selFatJet[:,0]]),
+#                "eta":  ak.Array([[v.eta  for v in sublist] for sublist in selev.selFatJet[:,0]]),
+#                "phi":  ak.Array([[v.phi  for v in sublist] for sublist in selev.selFatJet[:,0]]),
+#                "mass": ak.Array([[v.mass for v in sublist] for sublist in selev.selFatJet[:,0]]),
+#                #"jet_flavor": ak.Array([[v.jet_flavor for v in sublist] for sublist in splittings]),   # "bb"
+#                #"btag_string": ak.Array([[v.btag_string for v in sublist] for sublist in splittings]),  # str(particleNet_HbbvsQCD)
+#                "part_A": ak.zip(
+#                    {
+#                        "pt":         ak.Array([[v.pt   for v in sublist] for sublist in selev.selFatJet[:,0].subjets[:,0]]),
+#                        "eta":        ak.Array([[v.eta  for v in sublist] for sublist in selev.selFatJet[:,0].subjets[:,0]]),
+#                        "phi":        ak.Array([[v.phi  for v in sublist] for sublist in selev.selFatJet[:,0].subjets[:,0]]),
+#                        "mass":       ak.Array([[v.mass for v in sublist] for sublist in selev.selFatJet[:,0].subjets[:,0]]),
+#                        #"jet_flavor": ak.Array([[v.part_A.jet_flavor for v in sublist] for sublist in splittings]), # "b"
+#                        #"btag_string": ak.Array([[v.part_A.btag_string for v in sublist] for sublist in splittings]),  # str(btagDeepB)
+#                    },
+#                    with_name="PtEtaPhiMLorentzVector",
+#                    behavior=vector.behavior
+#                ),
+#                "part_B": ak.zip(
+#                    {
+#                        "pt":         ak.Array([[v.pt   for v in sublist] for sublist in selev.selFatJet[:,0].subjets[:,1]]),
+#                        "eta":        ak.Array([[v.eta  for v in sublist] for sublist in selev.selFatJet[:,0].subjets[:,1]]),
+#                        "phi":        ak.Array([[v.phi  for v in sublist] for sublist in selev.selFatJet[:,0].subjets[:,1]]),
+#                        "mass":       ak.Array([[v.mass for v in sublist] for sublist in selev.selFatJet[:,0].subjets[:,1]]),
+#                        #"jet_flavor": ak.Array([[v.part_B.jet_flavor for v in sublist] for sublist in splittings]),   # "b"
+#                        #"btag_string": ak.Array([[v.part_B.btag_string for v in sublist] for sublist in splittings]), # str(btagDeepB)
+#                    },
+#                    with_name="PtEtaPhiMLorentzVector",
+#                    behavior=vector.behavior
+#                ),
+#            },
+#            with_name="PtEtaPhiMLorentzVector",
+#            behavior=vector.behavior
+#        )
+
 
 
         #
@@ -121,21 +164,21 @@ class analysis(processor.ProcessorABC):
         #
 
         # Hacking the tag variable
-        event["fourTag"] = True
-        event['tag'] = ak.zip({
-            "fourTag": event.fourTag,
+        selev["fourTag"] = True
+        selev['tag'] = ak.zip({
+            "fourTag": selev.fourTag,
         })
 
 
         # Hack the region varable
-        event["SR"] = True
-        event["region"] = ak.zip({
-            "SR": event.SR,
+        selev["SR"] = True
+        selev["region"] = ak.zip({
+            "SR": selev.SR,
         })
 
-        event["weight"] = 1.0
+        selev["weight"] = 1.0
 
-        event["passNFatJets"]  = (ak.num(event.selFatJet) > 0)
+
 
         fill = Fill(process=processName, year=year, weight="weight")
         histCuts = ["passNFatJets"]
@@ -148,10 +191,18 @@ class analysis(processor.ProcessorABC):
                            )
 
 
+        print(f" SubJets: {selev.selFatJet.subjets}")
+        print(f" SubJets fields: {selev.selFatJet.subjets.fields}")
+        selev["selFatJet_subjets"] = selev.selFatJet.subjets
+        #print(f" SubJets pt: {selev.selFatJet_subjets.pt}")
+
         #
         # Jets
         #
         fill += Jet.plot(("fatJets", "Selected Fat Jets"),        "selFatJet",           skip=["deepjet_c"], bins={"pt": (50, 0, 1000)})
+
+        # print(f" SubJets pt {selev.selFatJet_subjets.pt[0:5]}\n")
+        # fill += Jet.plot(("subJets", "Selected Fat Jet SubJet"),   "selFatJet_subjets",  skip=["deepjet_c","deepjet_b","id_pileup","id_jet","n"], bins={"pt": (50, 0, 1000)})
 
 #        for _s_type in cleaned_splitting_name:
 #            fill += ClusterHists( (f"splitting_{_s_type}", f"{_s_type} Splitting"), f"splitting_{_s_type}" )
@@ -159,7 +210,7 @@ class analysis(processor.ProcessorABC):
         #
         # fill histograms
         #
-        fill(event, hist)
+        fill(selev, hist)
 
         processOutput = {}
 
