9695aa57bc9a0a46910c910eb8680451b940d696
John Alison <johnda102@gmail.com>
2025-06-10 17:26:24 -0400

    Add fixed binning YAML files for sideband plots and update script
    Updated YAML files with fixed binning for sideband plots (`SvB_MA_ps_hh` and `SvB_MA_noFvT_ps_hh`) and modified the plotting script to use these new files.

--- python/scripts/makeplotsForPaper.sh
+++ python/scripts/makeplotsForPaper.sh
@@ -1,6 +1,6 @@
 python plots/plot_from_yaml.py --input_yaml_files plotsForPaper/mixedVsDataVs3b/RunII/passPreSel/fourTag/SR/SvB_MA_noFvT_ps_hh_ave_logy_fixed_binning.yaml   -o plotsForPaper/mixedVsDataVs3b
 python plots/plot_from_yaml.py --input_yaml_files plotsForPaper/mixedVsDataVs3b/RunII/passPreSel/fourTag/SB/SvB_MA_noFvT_ps_hh_ave_logy_fixed_binning.yaml   -o plotsForPaper/mixedVsDataVs3b
 
-python plots/plot_from_yaml.py --input_yaml_files plotsForPaper/RunII/passPreSel/fourTag/SB/SvB_MA_ps_hh_logy.yaml -o plotsForPaper
-python plots/plot_from_yaml.py --input_yaml_files plotsForPaper/RunII/passPreSel/fourTag/SB/SvB_MA_noFvT_ps_hh_logy.yaml  -o plotsForPaper
+python plots/plot_from_yaml.py --input_yaml_files plotsForPaper/RunII/passPreSel/fourTag/SB/SvB_MA_ps_hh_logy_fixed_binning.yaml -o plotsForPaper
+python plots/plot_from_yaml.py --input_yaml_files plotsForPaper/RunII/passPreSel/fourTag/SB/SvB_MA_noFvT_ps_hh_logy_fixed_binning.yaml  -o plotsForPaper
 
