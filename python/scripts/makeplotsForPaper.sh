27f3f686caf07196f38b42b356365063378cdd13
John Alison <johnda102@gmail.com>
2025-06-11 16:04:26 -0400

    Adding postfit plots for paper

--- python/scripts/makeplotsForPaper.sh
+++ python/scripts/makeplotsForPaper.sh
@@ -4,3 +4,6 @@ python plots/plot_from_yaml.py --input_yaml_files plotsForPaper/mixedVsDataVs3b/
 python plots/plot_from_yaml.py --input_yaml_files plotsForPaper/RunII/passPreSel/fourTag/SB/SvB_MA_ps_hh_logy_fixed_binning.yaml -o plotsForPaper
 python plots/plot_from_yaml.py --input_yaml_files plotsForPaper/RunII/passPreSel/fourTag/SB/SvB_MA_noFvT_ps_hh_logy_fixed_binning.yaml  -o plotsForPaper
 
+
+python plots/plot_from_yaml.py --input_yaml_files plotsForPaper/SvB_MA_postfitplots_fit_b.yaml   -o plotsForPaper/
+python plots/plot_from_yaml.py --input_yaml_files plotsForPaper/SvB_MA_postfitplots_prefit.yaml   -o plotsForPaper/
